e3edead154e5919e00c0855b32b89a407860eb58
Riko Eksteen <reksteen@gmail.com>
2019-02-22 12:06:29 -0800

    Remove references to forked repo from FDC3 repo

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -99,7 +99,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: 'https://github.com/rikoe/FDC3'
+  repoUrl: 'https://github.com/FDC3/FDC3'
 };
 
 module.exports = siteConfig;
