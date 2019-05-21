d597eaec75b9709bf09aca20ed3851c4d8053023
Aaron Griswold <aaron@grizzwolf.com>
2019-05-21 12:07:25 -0400

    changed back trailing slashes - issues with links to img files. Also working on Use Cases section to change background colors of sections

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,9 +15,9 @@ const users = require('./data/users.json');
 const siteConfig = {
   title: 'FDC3', // Title for your website. THIS CHANGES THE TITLE TAGS FOR THE WEBSITE (LOOK IN BROWSER TAB AT TOP)
   tagline: 'Open standards for the financial desktop', //THIS CHANGES THE TITLE TAGS FOR THE WEBSITE (LOOK IN BROWSER TAB AT TOP)
-  url: 'https://grizzwolf.github.io/FDC3/',
+  url: 'https://grizzwolf.github.io/FDC3',
   //cname: 'fdc3.finos.org',
-  baseUrl: '/FDC3/',
+  baseUrl: '/',
   // For publishing to GitHub pages
   projectName: 'FDC3',
   organizationName: 'grizzwolf',
@@ -45,7 +45,8 @@ const siteConfig = {
   /* Colors for website */
   colors: {
     primaryColor: '#018CC1', /* This changes the top FINOS banner & all links - DO NOT CHANGE */
-    secondaryColor: '#0033A0' /* This does net change anything - but keep as the project / program main color */
+    secondaryColor: '#000000' /* This does net change anything - but keep as the project / program main color */
+  
   },
 
   /* Custom fonts for website */
