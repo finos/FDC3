b44bc69e3d2296da9f94beff730ba0b2f2a248a3
Aaron Griswold <aaron@grizzwolf.com>
2019-07-05 12:24:00 -0400

    reverting temporary changes made for base URL to merge with current PR96

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,12 +15,12 @@ const users = require('./data/users.json');
 const siteConfig = {
   title: 'FDC3', // Title for your website. THIS CHANGES THE TITLE TAGS FOR THE WEBSITE (LOOK IN BROWSER TAB AT TOP)
   tagline: 'Open standards for the financial desktop', //THIS CHANGES THE TITLE TAGS FOR THE WEBSITE (LOOK IN BROWSER TAB AT TOP)
-  url: 'https://grizzwolf.github.io/FDC3/',
-  //cname: 'fdc3.finos.org',
-  baseUrl: '/FDC3/',
+  url: 'https://fdc3.finos.org',
+  cname: 'fdc3.finos.org',
+  baseUrl: '/',
   // For publishing to GitHub pages
   projectName: 'FDC3',
-  organizationName: 'grizzwolf',
+  organizationName: 'FDC3',
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'fdc3-intro', label: 'Docs'},    
