86bbb5f9d9fde20af1f1f5f6cdc0a58ba6ef6b39
Aaron Griswold <aaron@grizzwolf.com>
2019-07-09 14:01:04 -0400

    added FDC3 Repo info in siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -94,7 +94,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: 'https://fdc3.finos.org/'
+  repoUrl: 'https://github.com/FDC3/FDC3'
 };
 
 module.exports = siteConfig;
