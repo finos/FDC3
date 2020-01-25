bab449f6ba7887e36bbf1909771973bc2ec86692
Riko Eksteen <reksteen@gmail.com>
2020-01-25 23:03:04 +0000

    Add context filtering to API and improve docs
    - Added overloads for addContextListener that include contextType parameter
    - Added optional contextType parameter to getContextValue
    - Rename Channel.addBroadcastListener to Channel.addContextListener for consistency
    - Change signature of channel context listener to match desktop agent one (remove redundant channel parameter)
    - Introduce ContextHandler shared type for context handlers like addContextListener.
    - Add documentation for API changes
    - Move Channel, Listener, DisplayMetadata and ContextHandler to their own pages
    - Cleanup of markdown docs

--- src/api/interface.ts
+++ src/api/interface.ts
@@ -4,6 +4,7 @@
  */
 
 type Context = object;
+type ContextHandler = (context: Context) => void;
 
 enum OpenError {
   AppNotFound = "AppNotFound",
@@ -90,10 +91,10 @@ declare class Channel {
   public readonly type: string;
 
   /**
-   * Channels may be visualized and selectable by users. DisplayMetaData may be used to provide hints on how to see them.
+   * Channels may be visualized and selectable by users. DisplayMetadata may be used to provide hints on how to see them.
    * For app channels, displayMetadata would typically not be present
    */
-  displayMetadata?: DisplayMetadata;
+  public readonly displayMetadata?: DisplayMetadata;
 
    /**
    * Broadcasts the given context on this channel. This is equivalent to joining the channel and then calling the 
@@ -102,34 +103,36 @@ declare class Channel {
    * Note that this function can be used without first joining the channel, allowing applications to broadcast on
    * channels that they aren't a member of.
    * 
-   * This broadcast will be received by all windows that are members of this channel, *except* for the window that
-   * makes the broadcast. This matches the behaviour of the top-level FDC3 `broadcast` function.
    * `Error` with a string from the `ChannelError` enumeration.
    */
   public broadcast(context: Context): Promise<void>;
 
   /**
    * Returns the last context that was broadcast on this channel. All channels initially have no context, until a 
-   * window is added to the channel and then broadcasts. If there is not yet any context on the channel, this method
-   * will return `null`. The context is also reset back into it's initial context-less state whenever a channel is
-   * cleared of all windows.
+   * context is broadcast on the channel. If there is not yet any context on the channel, this method
+   * will return `null`.
    * 
    * The context of a channel will be captured regardless of how the context is broadcasted on this channel - whether
    * using the top-level FDC3 `broadcast` function, or using the channel-level {@link broadcast} function on this 
    * object.
    * 
-   * NOTE: Only non-default channels are stateful, for the default channel this method will always return `null`.
+   * Optionally a {@link contextType} can be provided, in which case the current context of the matching type will
+   * be returned (if any). Desktop agent implementations may decide to record contexts by type, in which case it will
+   * be possible to get the most recent context of the type specified, but this is not guaranteed.
+   * 
    * `Error` with a string from the `ChannelError` enumeration.
    */
-  public getCurrentContext(): Promise<Context|null>;
+  public getCurrentContext(contextType?: string): Promise<Context|null>;
 
   /**
-   * Event that is fired whenever a window broadcasts on this channel.
-   * 
-   * The `channel` property within the event will always be this channel instance.
-   * `Error` with a string from the `ChannelError` enumeration.
+   * Adds a listener for incoming contexts whenever a broadcast happens on this channel.
    */
-  public addBroadcastListener(listener: (event: {channel: Channel; context: Context}) => void): Listener;
+  public addContextListener(handler: ContextHandler): Listener;
+
+  /**
+   * Adds a listener for incoming contexts of the specified context type whenever a broadcast happens on this channel.
+   */
+  public addContextListener(contextType: string, handler: ContextHandler): Listener;
 }
 
 /**
@@ -264,12 +267,17 @@ interface DesktopAgent {
   /**
    * Adds a listener for incoming Intents from the Agent.
    */
-  addIntentListener(intent: string, handler: (context: Context) => void): Listener;
+  addIntentListener(intent: string, handler: ContextHandler): Listener;
 
   /**
    * Adds a listener for incoming context broadcast from the Desktop Agent.
    */
-  addContextListener(handler: (context: Context) => void): Listener;
+  addContextListener(handler: ContextHandler): Listener;
+
+  /**
+   * Adds a listener for the broadcast of a specific type of context object.
+   */
+  addContextListener(contextType: string, handler: ContextHandler): Listener;
 
   /**
    * Retrieves a list of the System channels available for the app to join
@@ -277,9 +285,9 @@ interface DesktopAgent {
   getSystemChannels(): Promise<Array<Channel>>;
 
   /**
-   * Joins the app to the specified channel
-   * An app can only be joined to one channel at a time
-   * rejects with error if the channel is unavailable or the join request is denied
+   * Joins the app to the specified channel.
+   * An app can only be joined to one channel at a time.
+   * Rejects with error if the channel is unavailable or the join request is denied.
    * `Error` with a string from the `ChannelError` enumeration.
    */
   joinChannel(channelId: string) : Promise<void>;
@@ -293,6 +301,4 @@ interface DesktopAgent {
    * `Error` with a string from the `ChannelError` enumeration.
    */
   getOrCreateChannel(channelId: string): Promise<Channel>;
-
-
-
+}
