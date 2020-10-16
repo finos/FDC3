ac54931a3046a9052485ef9f5d4603248b22ada7
Matt Jamieson <matt@whitedog.io>
2020-10-16 17:10:41 +0100

    Export costs for DesktopAgent methods for ES6 usage

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@
  * Copyright 2019 FINOS FDC3 contributors - see NOTICE file
  */
 
+import { AppIntent } from './api/AppIntent';
+import { Channel } from './api/Channel';
+import { ContextHandler } from './api/ContextHandler';
 import { DesktopAgent } from './api/DesktopAgent';
+import { IntentResolution } from './api/IntentResolution';
+import { Listener } from './api/Listener';
+import { Context } from './context/ContextTypes';
 
 export * from './api/AppIntent';
 export * from './api/AppMetadata';
@@ -15,8 +21,8 @@ export * from './api/Errors';
 export * from './api/IntentMetadata';
 export * from './api/IntentResolution';
 export * from './api/Listener';
-export * from './context/ContextTypes';
 export * from './context/ContextType';
+export * from './context/ContextTypes';
 export * from './intents/Intents';
 
 declare global {
@@ -24,3 +30,71 @@ declare global {
     fdc3: DesktopAgent;
   }
 }
+
+export const open: (name: string, context?: Context) => Promise<void> = (
+  name,
+  context
+) => {
+  return window.fdc3.open(name, context);
+};
+
+export const findIntent: (
+  intent: string,
+  context?: Context
+) => Promise<AppIntent> = (intent, context) => {
+  return window.fdc3.findIntent(intent, context);
+};
+
+export const findIntentsByContext: (
+  context: Context
+) => Promise<Array<AppIntent>> = context => {
+  return window.fdc3.findIntentsByContext(context);
+};
+
+export const broadcast: (context: Context) => void = context => {
+  window.fdc3.broadcast(context);
+};
+
+export const raiseIntent: (
+  intent: string,
+  context: Context,
+  target?: string
+) => Promise<IntentResolution> = (intent, context, target) => {
+  return window.fdc3.raiseIntent(intent, context, target);
+};
+
+export const addIntentListener: (
+  intent: string,
+  handler: ContextHandler
+) => Listener = (intent, handler) => {
+  return window.fdc3.addIntentListener(intent, handler);
+};
+
+export const addContextListener: (
+  contextTypeOrHandler: string | ContextHandler,
+  handler?: ContextHandler
+) => Listener = (a, b) => {
+  if (typeof a !== 'function') {
+    return window.fdc3.addContextListener(a as string, b as ContextHandler);
+  } else {
+    return window.fdc3.addContextListener(a as ContextHandler);
+  }
+};
+
+export const getSystemChannels: () => Promise<Array<Channel>> = () => {
+  return window.fdc3.getSystemChannels();
+};
+
+export const joinChannel: (channelId: string) => Promise<void> = channelId => {
+  return window.fdc3.joinChannel(channelId);
+};
+
+export const getOrCreateChannel: (
+  channelId: string
+) => Promise<Channel> = channelId => {
+  return window.fdc3.getOrCreateChannel(channelId);
+};
+
+export const getCurrentChannel: () => Promise<Channel> = () => {
+  return window.fdc3.getCurrentChannel();
+};
