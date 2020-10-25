fcff145e2f510dae92e4e702edaa15a9d958a19c
Riko Eksteen <reksteen@gmail.com>
2020-10-25 14:08:54 +0000

    feat: streamline main readme for npm and update website versioning
    - add clickable FDC3 logo at top of README that goes to fdc3.finos.org
    - simplify description of FDC3 and link to the four specifications that make up the standard, and use cases
    - add installation and usage sections to the README that concisely demonstrate how to use FDC3
    - add a documentation section that explicitly links to the documentation website, without hardcoded version URLs
    - improve formatting of "Getting Involved" section to make it more readable
    - simplify the roadmap, removing things already achieved, and summarising to 5 steps
    - move the Contributing section to the end
    - website: remove previous workaround to "hard code" docusaurus urls to the latest version, meaning we can use unversioned links in the readme

--- website/utils/swapVersions.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs')
-const versions = require('../versions.json')
-const swapped = [versions[1], versions[0], ...versions.splice(2)]
-fs.writeFileSync('./versions.json', JSON.stringify(swapped, null, 2))
-
