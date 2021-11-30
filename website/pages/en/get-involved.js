09647a60029baa4976ad7be88d68bb5a8d1e1615
Juan Estrella <juan.estrella@finos.org>
2021-11-30 10:13:59 +0100

    Add training link to FDC3 website

--- website/pages/en/get-involved.js
+++ website/pages/en/get-involved.js
@@ -36,7 +36,8 @@ function Help(props) {
       You can also attend the quarterly 
       [FDC3 General Meeting](https://github.com/finos/FDC3/issues?q=label%3A%22General+Meeting%22): 
       you can find meeting details in the 
-      [FINOS Project Calendar](https://calendar.google.com/calendar/u/0/embed?src=finos.org_fac8mo1rfc6ehscg0d80fi8jig@group.calendar.google.com).`,
+      [FINOS Project Calendar](https://calendar.google.com/calendar/u/0/embed?src=finos.org_fac8mo1rfc6ehscg0d80fi8jig@group.calendar.google.com).
+      \n Take the FINOS <a href="https://www.edx.org/course/fdc3-interoperability-for-the-financial-desktop" target="_blank">Training</a>.`,
     },
     {
       title: `Participate in the Standard's ideation` ,
