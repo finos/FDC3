c0811bee977f1cd34f73a25a997df569c7bd4a83
Kris West <kris@cosaic.io>
2022-01-18 10:56:53 +0000

    updating app-directory schema by running yarn run v1.22.10 $ tsdx build ⠙ Creating entry file  77%                 1.1s, estimated 1.4s [2K[1A[2K[G✓ Creating entry file 1.1 secs ⠙ Building modules  3%                  0.2s, estimated 5.5s [2K[1A[2K[G⠹ Building modules  12%                 0.7s, estimated 5.5s [2K[1A[2K[G⠸ Building modules  15%                 0.8s, estimated 5.5s [2K[1A[2K[G⠼ Building modules  46%                 2.5s, estimated 5.5s [2K[1A[2K[G⠴ Building modules  57%                 3.1s, estimated 5.5s [2K[1A[2K[G⠦ Building modules  59%                 3.2s, estimated 5.5s [2K[1A[2K[G⠧ Building modules  60%                 3.3s, estimated 5.5s [2K[1A[2K[G⠇ Building modules  62%                 3.4s, estimated 5.5s [2K[1A[2K[G⠏ Building modules  64%                 3.5s, estimated 5.5s [2K[1A[2K[G⠋ Building modules  66%                 3.6s, estimated 5.5s [2K[1A[2K[G✓ Building modules 3.6 secs Done in 5.74s.

--- src/app-directory/specification/test/index.js
+++ src/app-directory/specification/test/index.js
@@ -4,21 +4,24 @@ const assert = require('assert');
 const exampleApplication = require('../examples/application/fdc3-workbench');
 
 (async () => {
-    try {
-        const api = await SwaggerParser.validate('../appd.yaml');
+  try {
+    const api = await SwaggerParser.validate('../appd.yaml');
 
-        console.log(`API name: ${api.info.title}, Version: ${api.info.version}`);
+    console.log(`API name: ${api.info.title}, Version: ${api.info.version}`);
 
-        const applicationSchema = api.components.schemas.Application;
+    const applicationSchema = api.components.schemas.Application;
 
-        const v = new Validator();
+    const v = new Validator();
 
-        const validatorResult = v.validate(exampleApplication, applicationSchema);
+    const validatorResult = v.validate(exampleApplication, applicationSchema);
 
-        assert(validatorResult.valid, `The example application definition does not comply with the Application schema: ${validatorResult.errors}`);
+    assert(
+      validatorResult.valid,
+      `The example application definition does not comply with the Application schema: ${validatorResult.errors}`
+    );
 
-        console.log('Successfully validated the specification and the example application definition!');
-    } catch (error) {
-        console.log(error.message || error);
-    }
+    console.log('Successfully validated the specification and the example application definition!');
+  } catch (error) {
+    console.log(error.message || error);
+  }
 })();
