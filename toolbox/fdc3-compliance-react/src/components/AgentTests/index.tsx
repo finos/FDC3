fd2816c7bcf6a0002b3d57557ace8a43bc590bd4
David Butt <dbutt@scottlogic.com>
2022-03-11 15:00:43 +0000

    Listen to Mocha events for pass/fail of tests

--- toolbox/fdc3-compliance-react/src/components/AgentTests/index.tsx
+++ toolbox/fdc3-compliance-react/src/components/AgentTests/index.tsx
@@ -45,12 +45,31 @@ export const AgentTests = observer(() => {
 	const classes = useStyles();
 
 	useEffect(() => {
-		mocha.setup("bdd");
+		// We're not really using the JSON output but it prevents
+		// it from trying to add to the mocha div
+		// This version of the setup isn't in the typescript defs
+		(mocha as any).setup({ ui: "bdd", reporter: "json" });
 		initTests();
 	}, []);
 
 	const handRunTests = () => {
-		mocha.run();
+		const runner = mocha.run();
+
+		// runner contains all the test info and event handlers
+		// The typescript defs say it is void but they lie
+		// runner.stats contains all the totals
+		// runner.suite contains test details
+		// But we can also add event handlers to it:
+
+		(runner as any).on("pass", (test: any) => {
+			console.log("This test passed!!!");
+			console.log(test);
+		});
+
+		(runner as any).on("fail", (test: any) => {
+			console.log("Oh no it failed.");
+			console.log(test);
+		});
 	};
 
 	return (
