30b24facbcc050f86579a15167a75075a46aa405
finos-admin <finos-admin@users.noreply.github.com>
2022-03-18 12:37:54 +0000

    Deploy website
    Deploy website version based on f885209c21885e2f954c076ed54ad4d506b0f73f

---
+++ toolbox/fdc3-explained/1.0/main.js
@@ -0,0 +1,46 @@
+// enable application when FDC3 is available
+document.addEventListener('DOMContentLoaded', () => {
+  fdc3Init(enablePage);
+});
+
+// check if FDC3 is available
+function fdc3Init(callback) {
+  let fdc3Tries = 10
+
+  const onFDC3Ready = () => {
+    if (window.fdc3) {
+      callback.call(this);
+    } else {
+      if (fdc3Tries > 0) {
+        fdc3Tries--;
+        window.setTimeout(onFDC3Ready, 100);
+      }
+    }
+  }
+
+  onFDC3Ready();
+}
+
+const providerDetails = document.getElementById("providerDetails")
+const broadcastButton = document.getElementById("broadcastButton")
+const broadcastText = document.getElementById("broadcastText")
+
+function enablePage() {
+  console.log('FDC3 is available');
+
+  if (window.FSBL) {
+    window.FSBL.getFSBLInfo().then(info => providerDetails.innerHTML = 'Available - Finsemble ' + info.FSBLVersion);
+  } else if (window.fin) {
+    providerDetails.innerHTML = 'Available - OpenFin ' + fin.desktop.getVersion();
+  } else {
+    providerDetails.innerHTML = 'Available - Unknown';
+  }
+
+  broadcastButton.disabled = false;
+  broadcastText.disabled = false;
+}
+
+function broadcastFDC3Context() {
+  var context = JSON.parse(broadcastText.value);
+  fdc3.broadcast(context);
+}
