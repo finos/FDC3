a74bd3a7612653066e598bbf1f6243e9f1239fe3
David Butt <dbutt@scottlogic.com>
2022-03-22 14:31:10 +0000

    Ensure joinchannel test also leaves channel afterwards

--- toolbox/fdc3-compliance/src/test/fdc3.joinChannel.ts
+++ toolbox/fdc3-compliance/src/test/fdc3.joinChannel.ts
@@ -1,6 +1,15 @@
 describe("fdc3.joinChannel", () => {
+  afterEach(async () => {
+    window.fdc3.leaveCurrentChannel();
+  });
+
   it("Method is callable", async () => {
-    const channels = window.fdc3.getSystemChannels();
-    await window.fdc3.joinChannel(channels[0]);
+    const channels = await window.fdc3.getSystemChannels();
+
+    if (channels.length > 0) {
+      await window.fdc3.joinChannel(channels[0].id);
+    } else {
+      throw Error("No system channels are available");
+    }
   });
 });
