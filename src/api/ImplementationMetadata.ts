11ce6f0e2b44e9d23f572962f7deffedf5671284
Kris West <kris@cosaic.io>
2022-06-23 12:21:55 +0100

    adding optional feature info in ImplementationMetadata to docs and compliance info

--- src/api/ImplementationMetadata.ts
+++ src/api/ImplementationMetadata.ts
@@ -6,22 +6,6 @@
 import { AppMetadata } from './AppMetadata';
 
 /**
- * Enumeration defining keys for optional API features that developers may need
- * to know are supported or not supported in the Desktop Agent Implementation
- * that they are working in.
- */
-export enum OptionalFeatures {
-  /** Used to indicate whether the exposure of 'origninating app metadata' for
-   *  context and intent messages is supported by the Desktop Agent.*/
-  OriginatingAppMetadata = 'OriginatingAppMetadata',
-
-  /** Used to indicate whether the optional `fdc3.joinUserChannel`,
-   *  `fdc3.getCurrentChannel` and `fdc3.leaveCurrentChannel` are implemented by
-   *  the Desktop Agent.*/
-  UserChannelMembershipAPIs = 'UserChannelMembershipAPIs',
-}
-
-/**
  * Metadata relating to the FDC3 Desktop Agent implementation and its provider.
  */
 export interface ImplementationMetadata {
@@ -40,7 +24,12 @@ export interface ImplementationMetadata {
    *  the Desktop Agent API.
    */
   readonly optionalFeatures: {
+    /** Used to indicate whether the exposure of 'origninating app metadata' for
+     *  context and intent messages is supported by the Desktop Agent.*/
     OriginatingAppMetadata: boolean;
+    /** Used to indicate whether the optional `fdc3.joinUserChannel`,
+     *  `fdc3.getCurrentChannel` and `fdc3.leaveCurrentChannel` are implemented by
+     *  the Desktop Agent.*/
     UserChannelMembershipAPIs: boolean;
   };
 
