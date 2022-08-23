8a3769ae7fc49a29209dc72de8a8529b03850a89
Kris West <kris@cosaic.io>
2022-08-23 13:31:39 +0100

    fix broken links in homepage

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -108,21 +108,21 @@ class Index extends React.Component {
       <Block background="white" layout="fourColumn">
         {[
           {
-            content: `Create a consistent developer experience by adhering to the [API](${docUrl('api/overview')}) standard`,
+            content: `Create a consistent developer experience by adhering to the [API](${docUrl('api/spec')}) standard`,
             image: `${baseUrl}img/feature-api.svg`,
             imageAlign: 'top',
             title: 'API',
             key: 'API',
           },
           {
-            content: `Use [standardized verbs](${docUrl('intents/overview')}) to instruct other apps to take an action`,
+            content: `Use [standardized verbs](${docUrl('intents/spec')}) to instruct other apps to take an action`,
             image: `${baseUrl}img/feature-intents.svg`,
             imageAlign: 'top',
             title: 'Intents',
             key: 'Intents',
           },
           {
-            content: `Share [context](${docUrl('context/overview')}) between apps to eliminate re-keying and streamline workflow`,
+            content: `Share [context](${docUrl('context/spec')}) between apps to eliminate re-keying and streamline workflow`,
             image: `${baseUrl}img/feature-context.svg`,
             imageAlign: 'top',
             title: 'Context Data',
