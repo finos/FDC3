de6595f7d97ffb1aa7dc7cf80ea19b6f8e2b79bb
Rob Moffat <rob@kite9.com>
2023-02-21 11:53:26 +0000

    fixed roadmap

--- website-v2/src/pages/get-involved.js
+++ website-v2/src/pages/get-involved.js
@@ -7,68 +7,145 @@
 
 const React = require('react');
 
-function Help(props) {
-  const {config: siteConfig, language = ''} = props;
-  const {baseUrl, docsUrl} = siteConfig;
-  const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-  const langPart = `${language ? `${language}/` : ''}`;
-  const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from "@theme/Layout";
+import Container from "../components/Container"
 
-  const supportLinks = [
-    {
-      title: `Collaborate with the Community`,
-      content: 
-        `Most FDC3 collaboration takes place in the [FDC3 GitHub repository](https://github.com/finos/FDC3), 
-        where you can raise [issues](https://github.com/finos/FDC3/issues), 
-        submit [pull requests](https://github.com/finos/FDC3/pulls) 
-        and view [meeting minutes](https://github.com/finos/FDC3/issues?q=label%3Ameeting+).`,
-    },
-    {
-      title: `Learn more about the Standard`,
-      content: 
-      `[Join the General List](mailto:fdc3+subscribe@finos.org) (and check out the 
-      [archives](https://groups.google.com/a/finos.org/forum/#!forum/fdc3)) to stay up to date with the project. 
-      You can also attend the quarterly 
-      [FDC3 General Meeting](https://github.com/finos/FDC3/issues?q=label%3A%22General+Meeting%22): 
-      you can find meeting details in the 
-      [FINOS Project Calendar](https://calendar.google.com/calendar/u/0/embed?src=finos.org_fac8mo1rfc6ehscg0d80fi8jig@group.calendar.google.com).
-      \n Take the free FDC3 <a href="https://www.edx.org/course/fdc3-interoperability-for-the-financial-desktop" target="_blank">Training</a>.`,
-    },
-    {
-      title: `Participate in the Standard's ideation` ,
-      content: 
-      `The [Standard Working Group](https://github.com/finos/FDC3/issues?q=label%3A%22Standard+WG+Meeting%22) 
-      meets once a month to shape the next version of the Standard: you can find meeting details in the 
-      [FINOS Project Calendar](https://calendar.google.com/calendar/u/0/embed?src=finos.org_fac8mo1rfc6ehscg0d80fi8jig@group.calendar.google.com). 
-      If you'd like to formally enroll as a voting Standard participant, 
-      please follow [these instructions](https://github.com/finos/fdc3#participate-in-the-standard-process).`,
-    },
-    {
-      title: `Are you using FDC3?`,
-      content: 
-      `If you are an existing user of the FDC3 Standard, we would love to hear from you: 
-      just email the [FDC3 General List](fdc3@finos.org) with details about how you are using it. 
-      If you'd like to be listed as a user on our [homepage](https://fdc3.finos.org), 
-      you can directly [send a pull request](https://github.com/finos/FDC3/edit/master/website/data/users.json) or, 
-      [contact us](fdc3-private@finos.org) if you need help with legal evaluation of your logo.`,
-    },
-  
-  ];
+const supportLinks = [
+  {
+    title: `Collaborate with the Community`,
+    content: 
+      `Most FDC3 collaboration takes place in the [FDC3 GitHub repository](https://github.com/finos/FDC3), 
+      where you can raise [issues](https://github.com/finos/FDC3/issues), 
+      submit [pull requests](https://github.com/finos/FDC3/pulls) 
+      and view [meeting minutes](https://github.com/finos/FDC3/issues?q=label%3Ameeting+).`,
+  },
+  {
+    title: `Learn more about the Standard`,
+    content: 
+    `[Join the General List](mailto:fdc3+subscribe@finos.org) (and check out the 
+    [archives](https://groups.google.com/a/finos.org/forum/#!forum/fdc3)) to stay up to date with the project. 
+    You can also attend the quarterly 
+    [FDC3 General Meeting](https://github.com/finos/FDC3/issues?q=label%3A%22General+Meeting%22): 
+    you can find meeting details in the 
+    [FINOS Project Calendar](https://calendar.google.com/calendar/u/0/embed?src=finos.org_fac8mo1rfc6ehscg0d80fi8jig@group.calendar.google.com).
+    \n Take the free FDC3 <a href="https://www.edx.org/course/fdc3-interoperability-for-the-financial-desktop" target="_blank">Training</a>.`,
+  },
+  {
+    title: `Participate in the Standard's ideation` ,
+    content: 
+    `The [Standard Working Group](https://github.com/finos/FDC3/issues?q=label%3A%22Standard+WG+Meeting%22) 
+    meets once a month to shape the next version of the Standard: you can find meeting details in the 
+    [FINOS Project Calendar](https://calendar.google.com/calendar/u/0/embed?src=finos.org_fac8mo1rfc6ehscg0d80fi8jig@group.calendar.google.com). 
+    If you'd like to formally enroll as a voting Standard participant, 
+    please follow [these instructions](https://github.com/finos/fdc3#participate-in-the-standard-process).`,
+  },
+  {
+    title: `Are you using FDC3?`,
+    content: 
+    `If you are an existing user of the FDC3 Standard, we would love to hear from you: 
+    just email the [FDC3 General List](fdc3@finos.org) with details about how you are using it. 
+    If you'd like to be listed as a user on our [homepage](https://fdc3.finos.org), 
+    you can directly [send a pull request](https://github.com/finos/FDC3/edit/master/website/data/users.json) or, 
+    [contact us](fdc3-private@finos.org) if you need help with legal evaluation of your logo.`,
+  }
+];
+
+const GridBlock = (props) => {
 
   return (
-    <div className="docMainWrapper wrapper">
-      <div className="mainContainer documentContainer postContainer">
-        <div className="post">
-          <header className="postHeader">
-            <h1>Get Involved in FDC3</h1>
-          </header>
-          <p>The FDC3 Standard is the result of the work of a dedicated group of contributors from both <a href="http://www.finos.org/members" target="_blank">FINOS members</a> and non-member contributing organizations alike. Ready to get involved in our community? Here are some ways to get started:
-          </p>
-          <GridBlock contents={supportLinks} layout="fourColumn" />
-        </div>
-      </div>
+    <div class="gridBlock">
+      {
+        supportLinks.map(sl =>
+          <div class="blockElement fourByGridBlock">
+            <div class="blockContent">
+              <h2>{sl.title}</h2>
+              <div><p>{sl.content}</p></div>
+            </div>
+          </div>
+        )
+      }
     </div>
-  );
+  )
 }
 
-export default Help
+export default () => {
+
+  const context = useDocusaurusContext();
+  const siteConfig = context.siteConfig;
+
+
+  return (
+    <Layout>
+      <Container>
+        <header className="postHeader">
+          <h1>Get Involved in FDC3</h1>
+        </header>
+        <GridBlock />
+      </Container>
+    </Layout>)
+};
+
+
+// export default (props) => {
+  
+//   const {config: siteConfig, language = ''} = props;
+//   const {baseUrl, docsUrl} = siteConfig;
+//   const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+//   const langPart = `${language ? `${language}/` : ''}`;
+//   const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+//   const supportLinks = [
+//     {
+//       title: `Collaborate with the Community`,
+//       content: 
+//         `Most FDC3 collaboration takes place in the [FDC3 GitHub repository](https://github.com/finos/FDC3), 
+//         where you can raise [issues](https://github.com/finos/FDC3/issues), 
+//         submit [pull requests](https://github.com/finos/FDC3/pulls) 
+//         and view [meeting minutes](https://github.com/finos/FDC3/issues?q=label%3Ameeting+).`,
+//     },
+//     {
+//       title: `Learn more about the Standard`,
+//       content: 
+//       `[Join the General List](mailto:fdc3+subscribe@finos.org) (and check out the 
+//       [archives](https://groups.google.com/a/finos.org/forum/#!forum/fdc3)) to stay up to date with the project. 
+//       You can also attend the quarterly 
+//       [FDC3 General Meeting](https://github.com/finos/FDC3/issues?q=label%3A%22General+Meeting%22): 
+//       you can find meeting details in the 
+//       [FINOS Project Calendar](https://calendar.google.com/calendar/u/0/embed?src=finos.org_fac8mo1rfc6ehscg0d80fi8jig@group.calendar.google.com).
+//       \n Take the free FDC3 <a href="https://www.edx.org/course/fdc3-interoperability-for-the-financial-desktop" target="_blank">Training</a>.`,
+//     },
+//     {
+//       title: `Participate in the Standard's ideation` ,
+//       content: 
+//       `The [Standard Working Group](https://github.com/finos/FDC3/issues?q=label%3A%22Standard+WG+Meeting%22) 
+//       meets once a month to shape the next version of the Standard: you can find meeting details in the 
+//       [FINOS Project Calendar](https://calendar.google.com/calendar/u/0/embed?src=finos.org_fac8mo1rfc6ehscg0d80fi8jig@group.calendar.google.com). 
+//       If you'd like to formally enroll as a voting Standard participant, 
+//       please follow [these instructions](https://github.com/finos/fdc3#participate-in-the-standard-process).`,
+//     },
+//     {
+//       title: `Are you using FDC3?`,
+//       content: 
+//       `If you are an existing user of the FDC3 Standard, we would love to hear from you: 
+//       just email the [FDC3 General List](fdc3@finos.org) with details about how you are using it. 
+//       If you'd like to be listed as a user on our [homepage](https://fdc3.finos.org), 
+//       you can directly [send a pull request](https://github.com/finos/FDC3/edit/master/website/data/users.json) or, 
+//       [contact us](fdc3-private@finos.org) if you need help with legal evaluation of your logo.`,
+//     },
+  
+//   ];
+
+//   return (
+//     <Layout>
+//       <Container>
+//           <header className="postHeader">
+//             <h1>Get Involved in FDC3</h1>
+//           </header>
+//           <p>The FDC3 Standard is the result of the work of a dedicated group of contributors from both <a href="http://www.finos.org/members" target="_blank">FINOS members</a> and non-member contributing organizations alike. Ready to get involved in our community? Here are some ways to get started:
+//           </p>
+//           <GridBlock contents={supportLinks} layout="fourColumn" />
+//       </Container>
+//     </Layout>
+//   );
+// }
