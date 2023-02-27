0d3a59695c9da7f360aa145c1c61a6454241fded
Rob Moffat <rob@kite9.com>
2023-02-27 12:04:49 +0000

    Removed the generated directory

--- website/.docusaurus/client-modules.js
+++ website/.docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require('/Users/rob/Documents/finos/FDC3/website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/rob/Documents/finos/FDC3/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/rob/Documents/finos/FDC3/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/rob/Documents/finos/FDC3/website/src/css/customTheme.css'),
+  require('/Users/rob/Documents/finos/fdc3-general/FDC3/website/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/rob/Documents/finos/fdc3-general/FDC3/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/rob/Documents/finos/fdc3-general/FDC3/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/rob/Documents/finos/fdc3-general/FDC3/website/src/css/customTheme.css'),
 ];
