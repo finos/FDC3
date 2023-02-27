0da6f2d88efdd2adaee52d57df442570a1242d09
Matt Jamieson <matt@whitedog.io>
2023-02-27 12:27:35 +0000

    chore: change static assets dir

--- toolbox/fdc3-workbench/vite.config.ts
+++ toolbox/fdc3-workbench/vite.config.ts
@@ -5,7 +5,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/toolbox/fdc3-workbench",
-	build: { outDir: "build" },
+	build: {
+		outDir: "build",
+		assetsDir: "static"
+	},
 	define: {
 		"process.env": "import.meta.env",
 		"process.platform": "({})",
