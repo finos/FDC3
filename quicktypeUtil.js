dfe3e6a3361306c3b340ad16d0e0df1a666d5a81
Tiago Pina <tiagoaspina@gmail.com>
2023-04-25 09:51:06 -0700

    new schema structure (#65)
    * new schema structure
    quicktypeUtils update
    npm scripts update
    
    * PR feedback
    
    * cleanup
    
    * typos
    
    * fix type generation folder
    
    * Fixed schema structures
    
    * copy schemas to website
    
    * package json update
    
    * removed defunct file
    
    * make sources optional
    
    * cleanup
    
    * Update schemas/bridging/raiseIntentResultResponse.schema.json
    
    Co-authored-by: Kris West <kris@cosaic.io>
    
    * cleanup
    
    * regenerated types
    
    * PR feedback
    
    * Fixe meta for requests
    
    * Apply suggestions from code review
    
    * Regenerating bridging typescript
    
    * Apply suggestions from code review
    
    * adjust OptionalFeatures and IntentResult schemas
    
    ---------
    
    Co-authored-by: Kris West <kris@cosaic.io>

--- quicktypeUtil.js
+++ quicktypeUtil.js
@@ -1,37 +1,51 @@
-/** Utility for preparing arguments to quicktype, which workaround a specific 
- * quicktype bug in command line argument handling (where a directory is used 
+/** Utility for preparing arguments to quicktype, which workaround a specific
+ * quicktype bug in command line argument handling (where a directory is used
  * as input the source language argument is ignored which causes our schemas
  * to be interpreted as JSON input, rather than JSONSchema).
- * Bug issue: 
+ * Bug issue:
  *  */
 
 const path = require('path');
 const fs = require('fs');
 const exec = require('child_process').exec;
 
-const args = process.argv;
-const inputFolder = args[2]
-const outputFile = args[3];
+const args = process.argv.slice(2);
+const outputFile = args.pop();
+const inputs = args;
 
-console.log("Input folder argument: " + inputFolder);
-console.log("Output file argument: " + outputFile);
+console.log('Inputs: ' + inputs.join(' | '));
+console.log('Output file argument: ' + outputFile);
 
-let srcs = "";
-fs.readdirSync(inputFolder).forEach(file => {
-    srcs += `--src ${path.join(inputFolder, file)} `;
-});
+let sources = '';
+
+let dirIndex = 0;
+
+while (dirIndex < inputs.length) {
+  if (inputs[dirIndex].endsWith('.schema.json')) {
+    sources += `--src ${path.join(inputs[dirIndex])} `;
+  } else {
+    fs.readdirSync(inputs[dirIndex], { withFileTypes: true }).forEach(file => {
+      if (file.isDirectory()) {
+        inputs.push(path.join(inputs[dirIndex], file.name));
+      } else {
+        sources += `--src ${path.join(inputs[dirIndex], file.name)} `;
+      }
+    });
+  }
+  dirIndex++;
+}
 
 // Normalise path to local quicktype executable.
 const quicktypeExec = ['.', 'node_modules', '.bin', 'quicktype'].join(path.sep);
 
-const command = `${quicktypeExec} -s schema -o ${outputFile} ${srcs}`;
-console.log("command to run: " + command);
+const command = `${quicktypeExec} -s schema -o ${outputFile} ${sources}`;
+console.log('command to run: ' + command);
 
 exec(command, function(error, stdout, stderr) {
-	if (stdout) {
-		console.log(stdout);
-	}
-	if (stderr) {
-		console.log(stderr);
-	}
+  if (stdout) {
+    console.log(stdout);
+  }
+  if (stderr) {
+    console.log(stderr);
+  }
 });
