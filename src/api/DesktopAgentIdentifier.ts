3e06fb568b3c3b22508062934f7fdd27f29d88d2
Kris West <kris@cosaic.io>
2023-04-25 19:48:36 +0100

    Add notes on generated sources to bridging spec

--- src/api/DesktopAgentIdentifier.ts
+++ src/api/DesktopAgentIdentifier.ts
@@ -9,7 +9,7 @@
  * response message is returned by the Desktop Agent (or more specifically its resolver)
  * rather than a specific app. Used as a substitute for `AppIdentifier` in cases where no
  * app details are available or are appropriate.
- * @experimental
+ * @experimental Introduced in FDC3 2.1 and may be refined by further changes outside the normal FDC3 versioning policy.
  */
 export interface DesktopAgentIdentifier {
   /** Used in Desktop Agent Bridging to attribute or target a message to a
