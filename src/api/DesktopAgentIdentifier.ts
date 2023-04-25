7e519fca77f472e07fbad673a4b051e745e58114
Tiago Pina <tiago.pina@cosaic.io>
2023-04-25 18:46:26 +0100

    added private channel schemas

--- src/api/DesktopAgentIdentifier.ts
+++ src/api/DesktopAgentIdentifier.ts
@@ -4,15 +4,15 @@
  */
 
 /**
- * Identifies a particular Desktop Agent in Desktop Agent Bridging scenarios 
- * where a request needs to be directed to a Desktop Agent rather than a specific app, or a 
- * response message is returned by the Desktop Agent (or more specifically its resolver) 
- * rather than a specific app. Used as a substitute for `AppIdentifier` in cases where no 
+ * Identifies a particular Desktop Agent in Desktop Agent Bridging scenarios
+ * where a request needs to be directed to a Desktop Agent rather than a specific app, or a
+ * response message is returned by the Desktop Agent (or more specifically its resolver)
+ * rather than a specific app. Used as a substitute for `AppIdentifier` in cases where no
  * app details are available or are appropriate.
  * @experimental
  */
 export interface DesktopAgentIdentifier {
-  /** Used in Desktop Agent Bridging to attribute or target a message to a 
+  /** Used in Desktop Agent Bridging to attribute or target a message to a
    *  particular Desktop Agent. */
   readonly desktopAgent: string;
 }
