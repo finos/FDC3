33da99dfa88bd207ef4c76b59eaa1a9d7e37e6fc
Rob Moffat <rob@kite9.com>
2023-06-12 18:39:01 +0100

    Debugging issue with conformance showcase 6

--- website/src/components/ConformanceShowcase/index.js
+++ website/src/components/ConformanceShowcase/index.js
@@ -14,31 +14,45 @@ export default ({ badge }) => {
                 }
             });
     });
+    
+	function transformBadge(badge) {
+		return (
+			<ul>
+				{
+					badge.items.map((item, key2) => {
+						return (<li key={key2}>
+							<p>{item.text} {(item.link) ? <em><a href={item.link}>More Details</a></em> : ""}</p>
+						</li>)
+					})
+				}
+			</ul>
+		)
+	}
+
+	function transformItem(conf, badge) {
+		return (
+			<div className={styles.conformanceShowcaseItem} key={key}>
+				{JSON.stringify(conf)}
+				<div className={styles.conformanceImage} someTag={conf.title + " " + conf.image}>
+					<img src={conf.image} alt={conf.title} title={conf.title} />
+				</div>
+				<div className={styles.conformanceText}>
+					<a title={conf.title} href={conf.infoLink}><div className="showcase-title">{conf.title}</div></a>
+					{
+						transformBadge(badge)
+					}
+				</div>
+			</div>
+		);
+	}
+	
 
     return <div className={styles.conformanceShowcase}>
         {
             relevant.map((c, key) => {
             	const conf = c.conf;
             	const badge = c.badge;
-                return (
-                    <div className={styles.conformanceShowcaseItem} key={key}>
-                       	{JSON.stringify(conf)}
-                        <div className={styles.conformanceImage} someTag={conf.title + " "+conf.image}>
-                            <img src={conf.image} alt={conf.title} title={conf.title} />
-                        </div>
-                        <div className={styles.conformanceText}>
-                            <a title={conf.title} href={conf.infoLink}><div className="showcase-title">{conf.title}</div></a>
-                            <ul>
-                                {
-                                    badge.items.map((item, key2) => {
-                                        return (<li key={key2}>
-                                            <p>{item.text} {  (item.link) ? <em><a href={item.link}>More Details</a></em> :"" }</p>
-                                        </li>)
-                                    })
-                                }
-                            </ul>
-                        </div>
-                    </div>)
+                return transformItem(conf, badge)
             })
         }
 
