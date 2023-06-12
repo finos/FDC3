d4254a4c92584793f28a75e1ad9c3cee2ca973e6
Rob Moffat <rob@kite9.com>
2023-06-12 18:51:38 +0100

    Debugging issue with conformance showcase 8

--- website/src/components/ConformanceShowcase/index.js
+++ website/src/components/ConformanceShowcase/index.js
@@ -14,45 +14,27 @@ export default ({ badge }) => {
                 }
             });
     });
-    
-	function transformBadge(badge) {
-		return (
-			<ul>
-				{
-					badge.items.map((item, key2) => {
-						return (<li key={key2}>
-							<p>{item.text} {(item.link) ? <em><a href={item.link}>More Details</a></em> : ""}</p>
-						</li>)
-					})
-				}
-			</ul>
-		)
-	}
-
-	function transformItem(conf, badge, key) {
-		return (
-			<div className={styles.conformanceShowcaseItem} key={key}>
-				{JSON.stringify(conf)}
-				<div className={styles.conformanceImage} someTag={conf.title + " " + conf.image}>
-					<img src={conf.image} alt={conf.title} title={conf.title} />
-				</div>
-				<div className={styles.conformanceText}>
-					<a title={conf.title} href={conf.infoLink}><div className="showcase-title">{conf.title}</div></a>
-					{
-						transformBadge(badge)
-					}
-				</div>
-			</div>
-		);
-	}
-	
 
     return <div className={styles.conformanceShowcase}>
         {
             relevant.map((c, key) => {
-            	const conf = c.conf;
-            	const badge = c.badge;
-                return transformItem(conf, badge, key)
+                return (
+                    <div className={styles.conformanceShowcaseItem} key={key}>
+                        <div className={styles.conformanceImage}>
+                            <img src={c.conf.image} alt={c.conf.title} title={c.conf.title} />
+                        </div>
+                        <div className={styles.conformanceText}>
+                            <a href={c.conf.infoLink}><div className="showcase-title">{c.conf.title}</div></a><ul>
+                                {
+                                    c.badge.items.map((item, key2) => {
+                                        return (<li className={styles.conformanceItem} key={key2}>
+                                            <p>{item.text} {  (item.link) ? <em><a href={item.link}>More Details</a></em> :"" }</p>
+                                        </li>)
+                                    })
+                                }
+                            </ul>
+                        </div>
+                    </div>)
             })
         }
 
