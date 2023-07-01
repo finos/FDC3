4131efb4bcf1720a8787be8553ddc36d185cc224
Rob Moffat <rob@kite9.com>
2023-07-01 23:06:24 +0100

    Added app3 use case
    Signed-off-by: Rob Moffat <rob@kite9.com>

--- src/lib/loaders/load-with-import.ts
+++ src/lib/loaders/load-with-import.ts
@@ -7,7 +7,7 @@ import { APIResponseMessage, FDC3Initialiser } from "../types";
 export function load(data: APIResponseMessage) : Promise<DesktopAgent> {
     return import(/* @vite-ignore */ data.url).then(ns => {
         const init = ns.default as FDC3Initialiser;
-        const da = init(data.appIdentifier);
+        const da = init(data.appIdentifier, data.daDetails);
         return da;
     })
 }
