69823fa9da430fc8125ccf6c3656ca03f8dc76b1
Rob Moffat <rob@kite9.com>
2023-07-04 14:30:22 +0100

    Tidied up code after @novavi's review

--- src/demo/app2.ts
+++ src/demo/app2.ts
@@ -1,4 +1,4 @@
-import { load } from '../lib/webc3'
+import { load } from '../lib/load'
 
 /**
  * This demonstrates using the API via a promise
@@ -7,7 +7,7 @@ load().then(fdc3 => {
     console.log("in promise")
     const log = document.getElementById("log");
     const msg = document.createElement("p");
-    msg.textContent = "FDC Loaded: "+JSON.stringify(fdc3.getInfo());
+    msg.textContent = "FDC3 Loaded: "+JSON.stringify(fdc3.getInfo());
     log?.appendChild(msg);
 
     fdc3.addContextListener(null, context => {
