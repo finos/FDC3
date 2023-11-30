aaf0880d4c8a097126ce02a43d276a4d03644c3c
Rob Moffat <rob@kite9.com>
2023-11-30 18:16:12 +0000

    Splitting out steps

--- packages/testing/test/step-definitions/channels.steps.ts
+++ packages/testing/test/step-definitions/channels.steps.ts
@@ -1,31 +1,5 @@
-import { TestMessaging } from '../support/TestMessaging';
-import { createDefaultChannels } from '../support/DefaultUserChannels';
-import { BasicDesktopAgent } from 'da';
-import { DefaultChannelSupport } from 'da';
-import { DefaultIntentSupport } from 'da';
-import { DefaultAppSupport } from 'da'
-import { Given, Then, When, World, setWorldConstructor } from '@cucumber/cucumber'
-import expect from 'expect';
-import { JSONPath } from 'jsonpath-plus'
-import { doesRowMatch, handleResolve, indexOf } from '../support/matching';
+import { Given, Then, When } from '@cucumber/cucumber'
 import { Context } from '@finos/fdc3';
-
-
-Given('A Basic API Setup', function() {
-
-  this.messaging = new TestMessaging()
-  this.defaultChannels = createDefaultChannels(this.messaging);
-
-  this.desktopAgent = new BasicDesktopAgent(
-      new DefaultChannelSupport(this.messaging, this.defaultChannels, null),
-      new DefaultIntentSupport(),
-      new DefaultAppSupport(),
-      "2.0",
-      "cucumber-provider"
-  )
-
-  this.result = null
-})
 
 Given('{string} pipes context to the result', function(contextHandlerName) {
   this[contextHandlerName] = (context: Context) => {
@@ -33,22 +7,6 @@ Given('{string} pipes context to the result', function(contextHandlerName) {
       this.result = [];
     }
     this.result.push(context)
-  }
-})
-
-When('I call the API {string}', async function(fnName: string) {
-    const fn = this.desktopAgent[fnName];
-    const result = await fn.call(this.desktopAgent)
-    if (result) {
-      this.result = result;
-    }
-})
-
-When('I call the API {string} with parameter {string}', async function(fnName: string, param: string) {
-  const fn = this.desktopAgent[fnName];
-  const result = await fn.call(this.desktopAgent, handleResolve(param, this))
-  if (result) {
-    this.result = result;
   }
 })
 
@@ -63,47 +21,4 @@ When('messaging receives a {string} with payload:', function (type, docString) {
   this.messaging.receive(message, this.log);
 });
 
-When('I call the API {string} with parameters {string} and {string}', async function (fnName: string, param1: string, param2: string) {
-  const fn = this.desktopAgent[fnName];
-  const result = await fn.call(this.desktopAgent, handleResolve(param1, this), handleResolve(param2, this))
-  if (result) {
-    this.result = result;
-  }
-});
-
-Then('the result is an array of objects with the following contents', function(params) {
-  const table = params.rawTable as string[][]
-  const headers = table.splice(0,1)[0]
-  const rowCount = table.length
-  this.log(`headers ${JSON.stringify(headers)} body ${JSON.stringify(table)} rows ${rowCount}`)
-
-  var resultCopy = JSON.parse(JSON.stringify(this.result)) as any[];
-  this.log(`result ${JSON.stringify(resultCopy)} length ${resultCopy.length}`)
-  expect(resultCopy).toHaveLength(rowCount);
-
-  resultCopy = resultCopy.filter(rr => {
-    const matchingRow = indexOf(table, headers, rr);
-    if (matchingRow != -1) {
-      return false
-    } else {
-      this.log(`Couldn't match row: ${JSON.stringify(rr)}`)
-      return true
-    }
-  })
-
-  expect(resultCopy).toHaveLength(0)
-});
-
-Then('the result is an object with the following contents', function (params) {
-  const table = params.rawTable as string[][]
-  const headers = table.splice(0,1)[0]
-  const rowCount = table.length
-  this.log(`headers ${JSON.stringify(headers)} body ${JSON.stringify(table)} rows ${rowCount}`)
-
-  expect(doesRowMatch(table[0], headers, this.result)).toBeTruthy();
-});
-
-Then('the result is null', function() {
-  expect(this.result).toBeNull()
-})
 
