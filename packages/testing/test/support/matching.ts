dd3bef52f7ad39e18d75f07f91252eee3e6bd65c
Rob Moffat <rob@kite9.com>
2023-11-30 14:54:12 +0000

    Added some more basic channel tests

---
+++ packages/testing/test/support/matching.ts
@@ -0,0 +1,23 @@
+import { JSONPath } from "jsonpath-plus";
+
+export function doesRowMatch(values: string[], headers: string[], data: any): boolean {
+    for (var i = 0; i < headers.length; i++) {
+        const found = JSONPath({ path: headers[i], json: data })[0];
+        const actual = values[i]
+        if (found != actual) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+export function indexOf(rows: string[][], headers: string[], data: any): number {
+    for (var i = 0; i < rows.length; i++) {
+        if (doesRowMatch(rows[i], headers, data)) {
+            return i;
+        }
+    }
+
+    return -1;
+}
