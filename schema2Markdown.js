d32900034e930ecd22a821c8bda779d889af0752
Juan Estrella <juan.estrella@finos.org>
2024-02-08 18:34:10 +0100

    refactor

--- schema2Markdown.js
+++ schema2Markdown.js
@@ -2,89 +2,119 @@ const fs = require('fs-extra');
 const yaml = require('js-yaml');
 const path = require('path');
 
-// Function to generate Markdown content from JSON schema
-function generateMarkdown(schema) {
-    const allOfArray = schema.allOf;
+function processProperty(propertyName, propertyDetails, schemaExamples) {
+    let markdownContent = '';
 
-    if (!Array.isArray(allOfArray) || allOfArray.length === 0) {
-        console.error('Invalid or empty "allOf" array in the JSON schema.');
-        return '';
-    }
+    if (propertyName === 'type') {
+        markdownContent += `### ${'Type'|| propertyName}\n\n`;
+        markdownContent += `\`${propertyDetails.const}\`\n\n`;
+    } else {
+        markdownContent += `### ${propertyDetails.title || propertyName}\n\n`;
+        markdownContent += `\`${propertyName}\`\n\n`;
+        markdownContent += `${propertyDetails.description}\n\n`;
 
-    const firstAllOf = allOfArray[0];
-    if (!firstAllOf || typeof firstAllOf !== 'object') {
-        console.error('Invalid or missing "allOf" object in the JSON schema.');
-        return '';
-    }
-
-    // Extract properties, required fields, and $ref from the first allOf object
-    const properties = firstAllOf.properties;
-    const required = firstAllOf.required;
-    const ref = firstAllOf.$ref;
-
-    if (!properties || typeof properties !== 'object') {
-        console.error('Invalid or missing "properties" object in the JSON schema.');
-        return '';
-    }
-
-    let markdownContent = `# ${schema.title}\n\n`;
-    markdownContent += `${schema.description}\n\n`; 
-
-
-    function processProperty(propertyName, propertyDetails) {
-        if (propertyName === 'type') {
-            markdownContent += `## ${'Type'|| propertyName}\n\n`;
-            markdownContent += `\`${propertyDetails.const}\`\n\n`;
-            markdownContent += `## Properties\n\n`; 
+        if (propertyDetails.type) {
+        markdownContent += `**Type**: ${propertyDetails.type}\n\n`;
         } else {
-            markdownContent += `### ${propertyDetails.title || propertyName}\n\n`;
-            markdownContent += `\`${propertyName}\`\n\n`;
-            markdownContent += `${propertyDetails.description}\n\n`;
-
-            if (propertyDetails.type) {
-            markdownContent += `**Type**: ${propertyDetails.type}\n\n`;
-            } else {
-                const contextRef = propertyDetails.properties?.context?.$ref || propertyDetails.$ref;
-                if (contextRef) {
-                    const reference = contextRef.toLowerCase().replace(/\s+/g, '-');
-                    markdownContent += `**Reference**: [${contextRef}](${reference}.md)\n\n`;
-                }
+            const contextRef = propertyDetails.properties?.context?.$ref || propertyDetails.$ref;
+            if (contextRef) {
+                const reference = contextRef.toLowerCase().replace(/\s+/g, '-');
+                markdownContent += `**Reference**: [${contextRef}](${reference}.md)\n\n`;
             }
+        }
 
-            if (propertyDetails.enum) {
-                markdownContent += `Possible values: ${propertyDetails.enum.join(', ')}\n\n`;
-            }
+        if (propertyDetails.enum) {
+            markdownContent += `Possible values: ${propertyDetails.enum.join(', ')}\n\n`;
+        }
 
-            if (propertyDetails.allOf) {
-                markdownContent += `**Reference**: ${propertyDetails.allOf.map((item) => item.$ref).join(', ')}\n\n`;
+        if (propertyDetails.allOf) {
+            markdownContent += `**Reference**: ${propertyDetails.allOf.map((item) => item.$ref).join(', ')}\n\n`;
+        }
+
+        if (schemaExamples) {
+            const example = schemaExamples[0];
+            if (example[propertyName]) {
+                markdownContent += `**Example Value**: \`'${example[propertyName]}'\`\n\n`;
             }
         }
     }
-
-    for (const [propertyName, propertyDetails] of Object.entries(properties)) {
-        processProperty(propertyName, propertyDetails);
-    }
-
-    // show required properties
-    if (required && required.length > 0) {
-        markdownContent += `### Required Properties:\n\n`;
-        // for each required property show the name
-        required.forEach((propertyName) => {
-            markdownContent += `* \`${propertyName}\`\n`;
-        });
-        markdownContent += '\n';
-    } 
-
-    if (ref) {
-        markdownContent += `ref: ${schema.allOf[1].$ref}\n\n`;
-    }
-
-    markdownContent += `## examples\n\n`;
-    markdownContent += '```json\n';
-    markdownContent += JSON.stringify(schema.examples, null, 2);
-    markdownContent += '\n```';
-
     return markdownContent;
+}
+
+function hasAllOf(allOfArray) {
+    return Array.isArray(allOfArray) && 
+        allOfArray.length > 0 && 
+        allOfArray[0] != null && 
+        allOfArray[0].properties != null
+}
+
+function hasProperties(schema) {
+    return schema.properties != null;
+}
+
+// Function to generate Markdown content from JSON schema
+function generateObjectMD(schema, title, schemaFile, schemaFolderName) {
+
+    // if (schemaFolderName === 'api') {
+    //     console.log('schema:', schema);
+    // }
+
+    const fileName = path.basename(schemaFile);
+
+    if (schema.title != null) {
+        title = schema.title;
+    }
+    let markdownContent = `# ${title}\n\n`;
+
+    if (schema.description != null) {
+        markdownContent += `${schema.description}\n\n`; 
+    }
+
+    console.log('hasAllOf/hasProperties', hasAllOf(schema.allOf), hasProperties(schema));
+    if (hasAllOf(schema.allOf) || hasProperties(schema)) {
+        // Extract properties, required fields, and $ref from the first allOf object
+        let root = schema;
+        if (hasAllOf(schema.allOf)) {
+            root = schema.allOf[0];
+        }
+        
+        const properties = root.properties;
+        const required = root.required;
+        const ref = root.$ref;
+
+        markdownContent += `## Properties\n\n`; 
+
+        for (const [propertyName, propertyDetails] of Object.entries(properties)) {
+            markdownContent += processProperty(propertyName, propertyDetails, schema.examples);
+        }
+
+        // show required properties
+        if (required && required.length > 0) {
+            markdownContent += `### Required Properties:\n\n`;
+            // for each required property show the name
+            required.forEach((propertyName) => {
+                markdownContent += `* \`${propertyName}\`\n`;
+            });
+            markdownContent += '\n';
+        } 
+
+        if (ref) {
+            markdownContent += `ref: ${schema.allOf[1].$ref}\n\n`;
+        }
+
+        if (schema.examples) {
+            markdownContent += `## examples\n\n`;
+            markdownContent += '```json\n';
+            markdownContent += JSON.stringify(schema.examples, null, 2);
+            markdownContent += '\n```';
+        }
+    }
+
+    const frontMatter = generateFrontMatter(fileName.replace('.schema.json', '.md'), schema.description);
+
+    const outputFileName = `./website/versioned_docs/version-2.1/${schemaFolderName}/schemas/${title.replace(/\s+/g, '')}.md`;
+    fs.outputFileSync(outputFileName, `---\n${yaml.dump(frontMatter)}\n---\n\n${markdownContent}`);
+    console.log(`Saved ${outputFileName}`);
 }
 
 function generateFrontMatter(title, description) {
@@ -95,36 +125,39 @@ function generateFrontMatter(title, description) {
     };
 }
 
-function processSchemaFile(schemaFile) {
+function processSchemaFile(schemaFile, schemaFolderName) {
     const schemaData = fs.readJSONSync(schemaFile);
 
-    const fileName = path.basename(schemaFile);
+    // if there is allOf, then it is an object
+    const allOfArray = schemaData.allOf;
+    if (Array.isArray(allOfArray) && allOfArray.length > 0) {
+        generateObjectMD(schemaData, null, schemaFile, schemaFolderName);
+        console.log(`Generated ${schemaFile} schema`);
+    }
+    if (schemaData.definitions) {
+        for (const [objectName, objectDetails] of Object.entries(schemaData.definitions)) {
+            generateObjectMD(objectDetails, objectName, schemaFile, schemaFolderName) + "\n\n";
+            console.log(`Generated ${schemaFolderName}/${objectName} schema`);
+        }
+    }
+}
 
-    const markdownContent = generateMarkdown(schemaData);
-    const frontMatter = generateFrontMatter(fileName.replace('.schema.json', '.md'), schemaData.description);
+function parseSchemaFolder(schemaFolderName) {
+    // Read all files in the schema folder
+    const schemaFiles = fs.readdirSync("./schemas/"+schemaFolderName)
+    .filter(file => file.endsWith('.json'))
+    .map(file => path.join("./schemas/"+schemaFolderName, file));
 
-    const outputFileName = `./docs/context/schemas/${schemaData.title.replace(/\s+/g, '')}.md`;
-    fs.outputFileSync(outputFileName, `---\n${yaml.dump(frontMatter)}\n---\n\n${markdownContent}`);
+    // Process each schema file
+    for (const schemaFile of schemaFiles) {
+        processSchemaFile(schemaFile, schemaFolderName);
+    }
 }
 
 function main() {
     // Folder containing JSON schema files
-    const schemaFolder = './schemas/context';
-
-    // Read all files in the schema folder
-    const schemaFiles = fs.readdirSync(schemaFolder)
-    .filter(file => file.endsWith('.json'))
-    .map(file => path.join(schemaFolder, file));
-
-    // Log number of schema files found
-    console.log(`Found ${schemaFiles.length} schema files.`);
-
-    // Process each schema file
-    schemaFiles.forEach(processSchemaFile);
-
-    // Log number of schema markdown files generated in the markdown folder
-    const markdownFiles = fs.readdirSync('./docs/context/schemas') || [];
-    console.log(`Generated ${markdownFiles.length} markdown files.`);
+    parseSchemaFolder('api');
+    parseSchemaFolder('context');
 }
 
 if (require.main === module) {
