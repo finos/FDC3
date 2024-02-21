9defb53b3ea680fee4115873567a364020d4e34c
Andrei Floricel <andrei.floricel@gmail.com>
2024-02-21 16:01:26 +0200

    Add deprecated types to maintain backward compatibility
    Signed-off-by: Andrei Floricel <andrei.floricel@gmail.com>

--- src/context/ContextType.ts
+++ src/context/ContextType.ts
@@ -44,3 +44,28 @@ export type ExperimentalContextType =
  * @see https://fdc3.finos.org/docs/context/spec
  */
 export type ContextType = StandardContextType | ExperimentalContextType | (string & {});
+
+/**
+ * @deprecated Use {@link StandardContextType} instead
+ */
+export enum ContextTypes {
+  Chart = 'fdc3.chart',
+  ChatInitSettings = 'fdc3.chat.initSettings',
+  ChatRoom = 'fdc3.chat.room',
+  Contact = 'fdc3.contact',
+  ContactList = 'fdc3.contactList',
+  Country = 'fdc3.country',
+  Currency = 'fdc3.currency',
+  Email = 'fdc3.email',
+  Instrument = 'fdc3.instrument',
+  InstrumentList = 'fdc3.instrumentList',
+  Interaction = 'fdc3.interaction',
+  Nothing = 'fdc3.nothing',
+  Organization = 'fdc3.organization',
+  Portfolio = 'fdc3.portfolio',
+  Position = 'fdc3.position',
+  ChatSearchCriteria = 'fdc3.chat.searchCriteria',
+  TimeRange = 'fdc3.timerange',
+  TransactionResult = 'fdc3.transactionResult',
+  Valuation = 'fdc3.valuation',
+}
