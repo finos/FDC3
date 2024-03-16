c9f6cd7b13637be3559455a7012a34b48a149457
Rob Moffat <rob@kite9.com>
2024-03-16 23:40:54 +0000

    Demo working again, cucumber broken

--- packages/da-proxy/src/listeners/DefaultIntentListener.ts
+++ packages/da-proxy/src/listeners/DefaultIntentListener.ts
@@ -1,6 +1,7 @@
 import { IntentHandler } from "@finos/fdc3";
 import { Messaging } from "../Messaging";
 import { AbstractListener } from "./AbstractListener";
+import { RaiseIntentAgentRequest } from "@finos/fdc3/dist/bridging/BridgingTypes";
 
 export class DefaultIntentListener extends AbstractListener<IntentHandler> {
 
@@ -16,7 +17,7 @@ export class DefaultIntentListener extends AbstractListener<IntentHandler> {
         this.intent = intent
     }
 
-    filter(m: any): boolean {
+    filter(m: RaiseIntentAgentRequest): boolean {
         return (m.type == 'raiseIntentRequest') && (m.payload.intent == this.intent)
     }
 
