81cd9be7877ca73ec5c664debf5a40414092d1b6
Rob Moffat <rob@kite9.com>
2024-04-23 14:11:42 +0100

    integrated grid

--- packages/sail2/src/client/index.tsx
+++ packages/sail2/src/client/index.tsx
@@ -1,6 +1,9 @@
-import { Frame } from "./frame"
+import { Frame } from "./frame/frame"
 import { createRoot } from 'react-dom/client';
+import { CLIENT_STATE } from "./state/state";
+import '../../static/fonts/DM_Sans/DM_Sans.css'
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
-root.render(<Frame />);
+const cs = CLIENT_STATE
+root.render(<Frame cs={cs} />);
