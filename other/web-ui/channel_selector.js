c901782dfc5bf0ab5d5f1715c004d84a24317a9e
Julianna Langston <julianna@cosaic.io>
2024-06-12 14:55:20 -0400

    Include web UI code

---
+++ other/web-ui/channel_selector.js
@@ -0,0 +1,33 @@
+const fillChannels = (data, messageClickedChannel) => {
+  const list = document.getElementById('list');
+  list.innerHTML = '';
+  data.forEach(({ id, displayMetadata }) => {
+    const node = document.createElement('div');
+    node.setAttribute('tabIndex', '0');
+    const span = document.createElement('span');
+    span.classList.add('glyph');
+    span.style.color = displayMetadata.color;
+    span.style.borderColor = displayMetadata.color;
+    span.textContent = displayMetadata.glyph ?? '';
+    node.appendChild(span);
+    const span2 = document.createElement('span');
+    span2.classList.add('name');
+    span2.textContent = displayMetadata.name;
+    node.appendChild(span2);
+    list.appendChild(node);
+    node.addEventListener('click', () => messageClickedChannel(id));
+  });
+};
+
+// STEP 1B: Receive port from parent
+window.addEventListener('message', ({ ports }) => {
+  // STEP 3B: Receive channel data from parent
+  ports[0].onmessage = ({ data }) => {
+    fillChannels(data, channel => {
+      // STEP 4A: Send user selection information to parent
+      ports[0].postMessage(channel);
+    });
+  };
+  // STEP 2A: Send confirmation over port to parent
+  ports[0].postMessage('Returning handshake');
+});
