29bc93fc212bcbfc764338255f59db3d09872880
Rob Moffat <rob@kite9.com>
2024-07-18 14:41:29 +0100

    Got about as far as possible with intent testing.

---
+++ fdc3-for-web-implementation/packages/client/test/support/responses/RaiseIntent.ts
@@ -0,0 +1,36 @@
+import { RaiseIntentAgentRequest, RaiseIntentAgentResponse } from "@finos/fdc3/dist/bridging/BridgingTypes";
+import { AutomaticResponse, TestMessaging } from "../TestMessaging";
+
+
+export class RaiseIntent implements AutomaticResponse {
+
+    filter(t: string) {
+        return t == 'raiseIntentRequest'
+    }
+
+    createRaiseIntentAgentResponseMessage(intentRequest: RaiseIntentAgentRequest, m: TestMessaging): RaiseIntentAgentResponse {
+        const out: RaiseIntentAgentResponse = {
+            meta: {
+                ...intentRequest.meta,
+                responseUuid: m.createUUID()
+            },
+            payload: {
+                intentResolution: {
+                    intent: intentRequest.payload.intent,
+                    source: intentRequest.payload.app
+                }
+            },
+            type: "raiseIntentResponse"
+        }
+
+        return out
+    }
+
+    action(input: object, m: TestMessaging) {
+        const intentRequest = input as RaiseIntentAgentRequest
+        // this sends out the intent resolution
+        const out1 = this.createRaiseIntentAgentResponseMessage(intentRequest, m)
+        setTimeout(() => { m.receive(out1) }, 100)
+        return Promise.resolve()
+    }
+}
