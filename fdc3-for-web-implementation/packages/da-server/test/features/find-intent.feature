ebd9ccdeab31ad761e666c53de5449f8bf7c8a63
Rob Moffat <rob@kite9.com>
2024-08-06 12:03:41 +0100

    Fixed findIntent tests

--- fdc3-for-web-implementation/packages/da-server/test/features/find-intent.feature
+++ fdc3-for-web-implementation/packages/da-server/test/features/find-intent.feature
@@ -1,62 +1,64 @@
 Feature: Find Intent API
 
   Background:
-    Given "libraryApp" is an app with the following intents
+    Given schemas loaded
+    And "libraryApp" is an app with the following intents
       | Intent Name | Context Type | Result Type      |
       | loanBook    | fdc3.book    | fdc3.loan        |
       | streamBook  | fdc3.book    | channel<chapter> |
       | returnBook  | fdc3.book    | {empty}          |
       | streamAny   | fdc3.book    | channel          |
     And A newly instantiated FDC3 Server
-    And "App/b1" is opened
+    And "App1/a1" is opened with connection id "abc"
+    And "App1/b1" is opened with connection id "def"
     And "App1/b1" registers an intent listener for "returnBook"
 
   Scenario: Unsuccessful Find Intents Request
     When "App1/a1" finds intents with intent "loanBook" and contextType "fdc3.instrument" and result type "{empty}"
     Then messaging will have outgoing posts
-      | msg.type           | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | to.instanceId |
+      | msg.matches_type   | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | to.instanceId |
       | findIntentResponse | loanBook                          |                                 0 | a1            |
 
   Scenario: Unsuccessful Find Intents Request With Result Type
     When "App1/a1" finds intents with intent "loanBook" and contextType "{empty}" and result type "unknownContext"
     Then messaging will have outgoing posts
-      | msg.type           | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | to.instanceId |
+      | msg.matches_type   | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | to.instanceId |
       | findIntentResponse | loanBook                          |                                 0 | a1            |
 
   Scenario: Successful Find Intents Request
     When "App1/a1" finds intents with intent "loanBook" and contextType "{empty}" and result type "{empty}"
     Then messaging will have outgoing posts
-      | msg.type           | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | msg.payload.appIntent.apps[0].appId | to.instanceId | msg.payload.appIntent.intent.displayName |
+      | msg.matches_type   | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | msg.payload.appIntent.apps[0].appId | to.instanceId | msg.payload.appIntent.intent.displayName |
       | findIntentResponse | loanBook                          |                                 1 | libraryApp                          | a1            | loan book                                |
 
   Scenario: Find Intents by Context Request
     When "App/a1" finds intents with contextType "fdc3.book"
     Then messaging will have outgoing posts
-      | msg.type                     | msg.payload.appIntents[0].intent.name | msg.payload.appIntents.length | to.instanceId | msg.payload.appIntents[0].intent.displayName |
+      | msg.matches_type             | msg.payload.appIntents[0].intent.name | msg.payload.appIntents.length | to.instanceId | msg.payload.appIntents[0].intent.displayName |
       | findIntentsByContextResponse | loanBook                              |                             4 | a1            | loan book                                    |
 
   Scenario: Successful Find Intents Request With Channel
     When "App1/a1" finds intents with intent "streamBook" and contextType "fdc3.book" and result type "channel"
     Then messaging will have outgoing posts
-      | msg.type           | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | msg.payload.appIntent.apps[0].appId | to.instanceId |
+      | msg.matches_type   | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | msg.payload.appIntent.apps[0].appId | to.instanceId |
       | findIntentResponse | streamBook                        |                                 1 | libraryApp                          | a1            |
 
   Scenario: Successful Find Intents Request With A Typed Channel
     When "App1/a1" finds intents with intent "streamBook" and contextType "{empty}" and result type "channel<chapter>"
     Then messaging will have outgoing posts
-      | msg.type           | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | msg.payload.appIntent.apps[0].appId | to.instanceId |
+      | msg.matches_type   | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | msg.payload.appIntent.apps[0].appId | to.instanceId |
       | findIntentResponse | streamBook                        |                                 1 | libraryApp                          | a1            |
 
   Scenario: Unsuccessful Find Intents Request With an untyped Channel
     When "App1/a1" finds intents with intent "streamAny" and contextType "{empty}" and result type "channel<spurious>"
     Then messaging will have outgoing posts
-      | msg.type           | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length |
+      | msg.matches_type   | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length |
       | findIntentResponse | streamAny                         |                                 0 |
 
   Scenario: Find Intent includes results for a running app with intent listener
     When "App1/a1" finds intents with intent "returnBook" and contextType "fdc3.book" and result type "{empty}"
     Then messaging will have outgoing posts
-      | msg.type           | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | to.instanceId |
+      | msg.matches_type   | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | to.instanceId |
       | findIntentResponse | returnBook                        |                                 2 | a1            |
     And messaging will have outgoing posts
       | msg.payload.appIntent.apps[1].appId | msg.payload.appIntent.apps[1].instanceId |
@@ -66,15 +68,16 @@ Feature: Find Intent API
       | libraryApp                          | {empty}                                  |
 
   Scenario: Disconnecting The Intent Listener
-    When "App1/b1" unsubscribes an intent listener for "returnBook"
+    When "App1/b1" unsubscribes an intent listener with id "uuid3"
     When "App1/a1" finds intents with intent "returnBook" and contextType "fdc3.book" and result type "{empty}"
     Then messaging will have outgoing posts
-      | msg.type           | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | to.instanceId | msg.payload.appIntent.apps[0].appId |
-      | findIntentResponse | returnBook                        |                                 1 | a1            | libraryApp                          |
+      | msg.matches_type                  | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | to.instanceId | msg.payload.appIntent.apps[0].appId |
+      | intentListenerUnsubscribeResponse | {null}                            | {null}                            | b1            | {null}                              |
+      | findIntentResponse                | returnBook                        |                                 1 | a1            | libraryApp                          |
 
   Scenario: Find Intent excludes results for a closed app with intent listener
     When "App1/b1" is closed
     When "App1/a1" finds intents with intent "returnBook" and contextType "fdc3.book" and result type "{empty}"
     Then messaging will have outgoing posts
-      | msg.type           | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | to.instanceId | msg.payload.appIntent.apps[0].appId |
+      | msg.matches_type   | msg.payload.appIntent.intent.name | msg.payload.appIntent.apps.length | to.instanceId | msg.payload.appIntent.apps[0].appId |
       | findIntentResponse | returnBook                        |                                 1 | a1            | libraryApp                          |
--- fdc3-for-web-implementation/packages/da-server/test/step-definitions/intents.steps.ts
+++ fdc3-for-web-implementation/packages/da-server/test/step-definitions/intents.steps.ts
@@ -1,8 +1,16 @@
-import { DataTable, Given } from "@cucumber/cucumber";
+import { DataTable, Given, When } from "@cucumber/cucumber";
 import { CustomWorld } from "../world";
 import { DirectoryApp } from "../../src/directory/DirectoryInterface";
-import { APP_FIELD } from "./generic.steps";
+import { APP_FIELD, contextMap, createMeta } from "./generic.steps";
 import { handleResolve } from "@kite9/testing";
+import {
+    FindIntentRequest,
+    FindIntentsByContextRequest,
+    AddIntentListenerRequest,
+    IntentListenerUnsubscribeRequest,
+    RaiseIntentRequest,
+    IntentResultRequest
+} from "@kite9/fdc3-common";
 
 type ListensFor = {
     [key: string]: {
@@ -58,115 +66,124 @@ Given('{string} is an app with the following intents', function (this: CustomWor
 
 });
 
-// When('{string} finds intents with intent {string} and contextType {string} and result type {string}', function (this: CustomWorld, appStr: string, intentName: string, contextType: string, resultType: string) {
-//     const meta = createMeta(this, appStr)
-//     const message = {
-//         meta,
-//         payload: {
-//             intent: handleResolve(intentName, this)!!,
-//             resultType: handleResolve(resultType, this),
-//             context: contextMap[contextType]
-//         },
-//         type: 'findIntentRequest'
-//     } as FindIntentAgentRequest
+When('{string} finds intents with intent {string} and contextType {string} and result type {string}', function (this: CustomWorld, appStr: string, intentName: string, contextType: string, resultType: string) {
+    const meta = createMeta(this, appStr)
+    const uuid = this.sc.getInstanceUUID(meta.source)!!
+    const message = {
+        meta,
+        payload: {
+            intent: handleResolve(intentName, this)!!,
+            resultType: handleResolve(resultType, this),
+            context: contextMap[contextType]
+        },
+        type: 'findIntentRequest'
+    } as FindIntentRequest
 
-//     this.server.receive(message, meta.source)
-// });
+    this.server.receive(message, uuid)
+});
 
-// When('{string} finds intents with contextType {string}', function (this: CustomWorld, appStr: string, contextType: string) {
-//     const meta = createMeta(this, appStr)
-//     const message = {
-//         meta,
-//         payload: {
-//             context: contextMap[contextType]
-//         },
-//         type: 'findIntentsByContextRequest'
-//     } as FindIntentsByContextAgentRequest
+When('{string} finds intents with contextType {string}', function (this: CustomWorld, appStr: string, contextType: string) {
+    const meta = createMeta(this, appStr)
+    const uuid = this.sc.getInstanceUUID(meta.source)!!
+    const message = {
+        meta,
+        payload: {
+            context: contextMap[contextType]
+        },
+        type: 'findIntentsByContextRequest'
+    } as FindIntentsByContextRequest
 
-//     this.server.receive(message, meta.source)
-// });
+    this.server.receive(message, uuid)
+});
 
-// Given('{string} registers an intent listener for {string}', function (this: CustomWorld, appStr: string, intent: string) {
-//     const meta = createMeta(this, appStr)
-//     const message = {
-//         type: 'onAddIntentListener',
-//         meta,
-//         payload: {
-//             intent: handleResolve(intent, this)
-//         }
-//     } as OnAddIntentListenerAgentRequest
-//     this.server.receive(message, meta.source)
-// });
+Given('{string} registers an intent listener for {string}', function (this: CustomWorld, appStr: string, intent: string) {
+    const meta = createMeta(this, appStr)
+    const uuid = this.sc.getInstanceUUID(meta.source)!!
 
-// Given('{string} registers an intent listener for {string} with contextType {string}', function (this: CustomWorld, appStr: string, intent: string, contextType: string) {
-//     const meta = createMeta(this, appStr)
-//     const message = {
-//         type: 'onAddIntentListener',
-//         meta,
-//         payload: {
-//             intent: handleResolve(intent, this),
-//             contextType: handleResolve(contextType, this)
-//         }
-//     } as OnAddIntentListenerAgentRequest
-//     this.server.receive(message, meta.source)
-// });
+    const message = {
+        type: 'addIntentListenerRequest',
+        meta,
+        payload: {
+            intent: handleResolve(intent, this)
+        }
+    } as AddIntentListenerRequest
+    this.server.receive(message, uuid)
+});
 
-
-// Given('{string} unsubscribes an intent listener for {string}', function (this: CustomWorld, appStr: string, intent: string) {
-//     const meta = createMeta(this, appStr)
-//     const message = {
-//         type: 'onUnsubscribeIntentListener',
-//         meta,
-//         payload: {
-//             intent: handleResolve(intent, this),
-//         }
-//     } as OnUnsubscribeIntentListenerAgentRequest
-//     this.server.receive(message, meta.source)
-// });
-
-// function raise(cw: CustomWorld, intentName: string, contextType: string, dest: string, meta: any): RaiseIntentAgentRequest {
-//     const destMeta = createMeta(cw, dest)
-//     const message = {
-//         type: 'raiseIntentRequest',
-//         meta: {
-//             ...meta,
-//             destination: {
-//                 ...destMeta.source,
-//                 desktopAgent: 'n/a'
-//             }
-//         },
-//         payload: {
-//             intent: handleResolve(intentName, cw),
-//             context: contextMap[contextType],
-//             app: destMeta.source
-//         }
-//     } as RaiseIntentAgentRequest
-//     return message;
-// }
-
-// When('{string} raises an intent for {string} with contextType {string} on app {string}', function (this: CustomWorld, appStr: string, intentName: string, contextType: string, dest: string) {
-//     const meta = createMeta(this, appStr)
-//     const message = raise(this, intentName, contextType, dest, meta)
-//     this.server.receive(message, meta.source)
-// });
-
-// When('{string} raises an intent for {string} with contextType {string} on app {string} with requestUuid {string}', function (this: CustomWorld, appStr: string, intentName: string, contextType: string, dest: string, requestUuid: string) {
-//     const meta = {
-//         ...createMeta(this, appStr), requestUuid
-//     }
-//     const message = raise(this, intentName, contextType, dest, meta)
-//     this.server.receive(message, meta.source)
-// })
+Given('{string} registers an intent listener for {string} with contextType {string}', function (this: CustomWorld, appStr: string, intent: string, contextType: string) {
+    const meta = createMeta(this, appStr)
+    const uuid = this.sc.getInstanceUUID(meta.source)!!
+    const message = {
+        type: 'addIntentListenerRequest',
+        meta,
+        payload: {
+            intent: handleResolve(intent, this),
+            contextType: handleResolve(contextType, this)
+        }
+    } as AddIntentListenerRequest
+    this.server.receive(message, uuid)
+});
 
 
-// When('we wait for the intent timeout', function (this: CustomWorld) {
-//     return new Promise<void>((resolve, _reject) => {
-//         setTimeout(() => resolve(), 2100)
-//     })
-// });
+Given('{string} unsubscribes an intent listener with id {string}', function (this: CustomWorld, appStr: string, id: string) {
+    const meta = createMeta(this, appStr)
+    const uuid = this.sc.getInstanceUUID(meta.source)!!
+    const message = {
+        type: 'intentListenerUnsubscribeRequest',
+        meta,
+        payload: {
+            listenerUUID: handleResolve(id, this),
+        }
+    } as IntentListenerUnsubscribeRequest
+    this.server.receive(message, uuid)
+});
+
+function raise(cw: CustomWorld, intentName: string, contextType: string, dest: string, meta: any): RaiseIntentRequest {
+    const destMeta = createMeta(cw, dest)
+    const message = {
+        type: 'raiseIntentRequest',
+        meta: {
+            ...meta,
+            destination: {
+                ...destMeta.source,
+                desktopAgent: 'n/a'
+            }
+        },
+        payload: {
+            intent: handleResolve(intentName, cw),
+            context: contextMap[contextType],
+            app: destMeta.source
+        }
+    } as RaiseIntentRequest
+    return message;
+}
+
+When('{string} raises an intent for {string} with contextType {string} on app {string}', function (this: CustomWorld, appStr: string, intentName: string, contextType: string, dest: string) {
+    const meta = createMeta(this, appStr)
+    const uuid = this.sc.getInstanceUUID(meta.source)!!
+    const message = raise(this, intentName, contextType, dest, meta)
+    this.server.receive(message, uuid)
+});
+
+When('{string} raises an intent for {string} with contextType {string} on app {string} with requestUuid {string}', function (this: CustomWorld, appStr: string, intentName: string, contextType: string, dest: string, requestUuid: string) {
+    const meta = {
+        ...createMeta(this, appStr), requestUuid
+    }
+    const uuid = this.sc.getInstanceUUID(meta.source)!!
+    const message = raise(this, intentName, contextType, dest, meta)
+    this.server.receive(message, uuid)
+})
+
+
+When('we wait for the intent timeout', function (this: CustomWorld) {
+    return new Promise<void>((resolve, _reject) => {
+        setTimeout(() => resolve(), 2100)
+    })
+});
 
 // When('{string} sends a raiseIntentResponse for intent {string} with requestUuid {string}', function (this: CustomWorld, appStr: string, intentName: string, requestUuid: string) {
 //     const meta = createMeta(this, appStr)
+//     const uuid = this.sc.getInstanceUUID(meta.source)!!
 //     const message = {
 //         type: 'raiseIntentResponse',
 //         meta: {
@@ -182,28 +199,29 @@ Given('{string} is an app with the following intents', function (this: CustomWor
 //                 }
 //             }
 //         }
-//     } as RaiseIntentAgentResponse
-//     this.server.receive(message, meta.source)
+//     } as RaiseIntentResponse
+//     this.server.receive(message, uuid)
 // });
 
-// When('{string} sends a raiseIntentResultResponse with requestUuid {string}', function (this: CustomWorld, appStr: string, requestUuid: string) {
-//     const meta = createMeta(this, appStr)
-//     const message = {
-//         type: 'raiseIntentResultResponse',
-//         meta: {
-//             requestUuid,
-//             responseUuid: this.sc.createUUID(),
-//             timestamp: new Date()
-//         },
-//         payload: {
-//             intentResult: {
-//                 context: {
-//                     "type": "fdc3.something",
-//                     "name": "Some Name"
-//                 }
-//             }
-//         }
-//     } as RaiseIntentResultAgentResponse
+When('{string} sends a intentResultRequest with requestUuid {string}', function (this: CustomWorld, appStr: string, requestUuid: string) {
+    const meta = createMeta(this, appStr)
+    const uuid = this.sc.getInstanceUUID(meta.source)!!
+    const message = {
+        type: 'intentResultRequest',
+        meta: {
+            requestUuid,
+            responseUuid: this.sc.createUUID(),
+            timestamp: new Date()
+        },
+        payload: {
+            intentResult: {
+                context: {
+                    "type": "fdc3.something",
+                    "name": "Some Name"
+                }
+            }
+        }
+    } as IntentResultRequest
 
-//     this.server.receive(message, meta.source)
-// });
+    this.server.receive(message, uuid)
+});
