d67f32b17239f911cdf5482805fe17bf4aa9ea2d
Rob Moffat <rob@kite9.com>
2024-11-19 15:12:13 +0000

    Allowing channel selector drag

--- toolbox/fdc3-for-web/demo/src/client/ui/channel-selector.ts
+++ toolbox/fdc3-for-web/demo/src/client/ui/channel-selector.ts
@@ -1,5 +1,6 @@
 import { BrowserTypes } from "@kite9/fdc3-schema";
 import { FDC3_USER_INTERFACE_CHANNEL_SELECTED_TYPE, FDC3_USER_INTERFACE_CHANNELS_TYPE, FDC3_USER_INTERFACE_HANDSHAKE_TYPE, FDC3_USER_INTERFACE_HELLO_TYPE, FDC3_USER_INTERFACE_RESTYLE_TYPE } from "@kite9/fdc3-schema/generated/api/BrowserTypes";
+import { dragElement } from "./drag";
 
 type IframeChannels = BrowserTypes.Fdc3UserInterfaceChannels
 type IframeRestyle = BrowserTypes.Fdc3UserInterfaceRestyle
@@ -9,30 +10,35 @@ var channels: any[] = []
 var channelId: string | null = null
 
 
-const DEFAULT_COLLAPSED_CSS = {
+export const DEFAULT_COLLAPSED_CSS = {
+    width: "50px",
+    height: "50px",
     position: "fixed",
     zIndex: "1000",
-    right: "10px",
-    bottom: "10px",
-    width: "50px",
-    height: "50px"
 }
 
 const DEFAULT_EXPANDED_CSS = {
+    width: "450px",
+    height: "600px",
     position: "fixed",
     zIndex: "1000",
-    right: "10px",
-    bottom: "10px",
-    width: "450px",
-    maxHeight: "600px",
     transition: "all 0.5s ease-out allow-discrete"
 }
 
+export type Position = {
+    right: string,
+    bottom: string
+}
+
+
+const position: Position = {
+    right: "10px",
+    bottom: "10px",
+}
 
 window.addEventListener("load", () => {
     const parent = window.parent;
     const logo = document.getElementById("logo")!!
-    const list = document.getElementById("channel-list")!!
     const close = document.getElementById("close")!!
 
     const mc = new MessageChannel();
@@ -43,21 +49,23 @@ window.addEventListener("load", () => {
     parent.postMessage({
         type: FDC3_USER_INTERFACE_HELLO_TYPE,
         payload: {
-            initialCSS: DEFAULT_COLLAPSED_CSS,
+            initialCSS: {
+                ...DEFAULT_COLLAPSED_CSS, ...position
+            },
             implementationDetails: "Demo Channel Selector v1.0"
         }
     } as any as IframeHello, "*", [mc.port2]);
 
     function changeSize(expanded: boolean) {
         document.body.setAttribute("data-expanded", "" + expanded);
-        myPort.postMessage({ type: FDC3_USER_INTERFACE_RESTYLE_TYPE, payload: { updatedCSS: expanded ? DEFAULT_EXPANDED_CSS : DEFAULT_COLLAPSED_CSS } } as IframeRestyle)
+        myPort.postMessage({ type: FDC3_USER_INTERFACE_RESTYLE_TYPE, payload: { updatedCSS: expanded ? { ...DEFAULT_EXPANDED_CSS, ...position } : { ...DEFAULT_COLLAPSED_CSS, ...position } } } as IframeRestyle)
     }
 
     myPort.addEventListener("message", (e) => {
         console.log(e.data.type)
         if (e.data.type == FDC3_USER_INTERFACE_HANDSHAKE_TYPE) {
             // ok, port is ready, send the iframe position detials
-            myPort.postMessage({ type: FDC3_USER_INTERFACE_RESTYLE_TYPE, payload: { updatedCSS: DEFAULT_COLLAPSED_CSS } } as IframeRestyle)
+            myPort.postMessage({ type: FDC3_USER_INTERFACE_RESTYLE_TYPE, payload: { updatedCSS: { ...DEFAULT_COLLAPSED_CSS, ...position } } } as IframeRestyle)
         } else if (e.data.type == FDC3_USER_INTERFACE_CHANNELS_TYPE) {
             const details = e.data as IframeChannels
             console.log(JSON.stringify("CHANNEL DETAILS: " + JSON.stringify(details)))
@@ -69,7 +77,8 @@ window.addEventListener("load", () => {
         }
     })
 
-    logo.addEventListener("click", () => {
+
+    dragElement(logo, myPort) /*, () => {
         list.innerHTML = ''
         channels.forEach(channel => {
 
@@ -93,7 +102,7 @@ window.addEventListener("load", () => {
 
         // ask the parent container to increase the window size
         changeSize(true)
-    })
+    }) */
 
     close.addEventListener("click", () => {
         changeSize(false)
