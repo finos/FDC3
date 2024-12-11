9ff3dcc5fb1cbe0d9ae841e79b6e68b1e7cded80
Kris West <kris.west@interop.io>
2024-12-11 10:20:24 +0000

    lint free fdc3-get-agent

--- packages/fdc3-get-agent/src/ui/NullIntentResolver.ts
+++ packages/fdc3-get-agent/src/ui/NullIntentResolver.ts
@@ -1,11 +1,7 @@
-import { AppIntent } from '@kite9/fdc3-standard';
 import { IntentResolver, IntentResolutionChoice } from '@kite9/fdc3-standard';
-import { Context } from '@kite9/fdc3-context';
 
 export class NullIntentResolver implements IntentResolver {
   async disconnect(): Promise<void> {}
-
   async connect(): Promise<void> {}
-
-  async chooseIntent(_appIntents: AppIntent[], _ctx: Context): Promise<IntentResolutionChoice | void> {}
+  async chooseIntent(): Promise<IntentResolutionChoice | void> {}
 }
