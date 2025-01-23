3f4797b243cd10cb11f378f2a46644fe4f4ff019
Rob Moffat <rob@kite9.com>
2025-01-23 12:23:24 +0000

    Remove unused files originally written for early development

--- toolbox/fdc3-for-web/demo/src/client/da/util.ts
+++ toolbox/fdc3-for-web/demo/src/client/da/util.ts
@@ -14,7 +14,7 @@ export const UI_URLS = {
     channelSelectorUrl: window.location.origin + '/static/da/channel-selector.html',
   },
   [UI.DEFAULT]: {
-    /* REPLACE THIS URL WITH fdc3.finos.org BEFORE RELEASE */
+    /* REPLACE THIS URL WITH true BEFORE RELEASE */
     intentResolverUrl: 'https://deploy-preview-1309--fdc3.netlify.app/toolbox/fdc3-reference-ui/intent_resolver.html',
     channelSelectorUrl: 'https://deploy-preview-1309--fdc3.netlify.app/toolbox/fdc3-reference-ui/channel_selector.html',
   },
