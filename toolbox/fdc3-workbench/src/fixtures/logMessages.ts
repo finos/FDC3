d8a0b617c6ba9a1fd9b1e5d80c99375fa8a8aed4
Rob Moffat <rob@kite9.com>
2025-02-25 12:01:17 +0000

    Fixed linting issues

--- toolbox/fdc3-workbench/src/fixtures/logMessages.ts
+++ toolbox/fdc3-workbench/src/fixtures/logMessages.ts
@@ -2,117 +2,117 @@
  * SPDX-License-Identifier: Apache-2.0
  * Copyright FINOS FDC3 contributors - see NOTICE file
  */
-import { logMessagesName, logMessagesType } from "../store/SystemLogStore";
+import { logMessagesName, logMessagesType } from '../store/SystemLogStore';
 
 type LogMessages = Record<logMessagesName, Partial<Record<logMessagesType, string>>>;
 
-export const getLogMessage = (name: logMessagesName, type: logMessagesType, value: string = ""): string => {
-	const logMessages: LogMessages = {
-		getFdc3: {
-			error: `The FDC3 API is not ready${value ? ` (${value})` : ""}`,
-		},
-		getChannels: {
-			success: `Retrieved User channels`,
-			error: `Failed to retrieve User channels`,
-		},
-		getCurrentChannel: {
-			success: `Retrieved current channel [${value}]`,
-			error: `Failed to retrieve current channel`,
-		},
-		joinUserChannel: {
-			success: `Joined the [${value}] channel`,
-			error: `Failed to join the [${value}] channel`,
-		},
-		leaveChannel: {
-			success: `Left the [${value}] channel`,
-			error: `Failed to leave the [${value}] channel`,
-			warning: `Not currently joined to a channel`,
-		},
-		broadcast: {
-			success: "Broadcast context",
-			error: "Failed to broadcast context:",
-		},
-		appBroadcast: {
-			success: `Broadcast context to channel [${value}]: `,
-			error: `Failed to broadcast context to channel [${value}]:`,
-		},
-		raiseIntent: {
-			success: `Raised intent [${value}]`,
-			error: `Failed to raise intent [${value}]`,
-		},
-		raiseIntentForContext: {
-			success: `Raised intent for context`,
-			error: `Failed to raise intent for context`,
-		},
-		addContextListener: {
-			success: `A context listener for '[${value}]' has been added`,
-			error: `Failed to add a context listener for '[${value}]'`,
-		},
-		removeContextListener: {
-			success: `A context listener for '[${value}]' has been removed`,
-			error: `Failed to remove a context listener for '[${value}]'`,
-		},
-		removeAppChannelContextListener: {
-			success: `${value}`,
-			error: `${value}`,
-		},
-		addIntentListener: {
-			success: `An intent listener for '[${value}]' has been added`,
-			error: `Failed to add an intent listener for '[${value}]'`,
-		},
-		removeIntentListener: {
-			success: `An intent listener for '[${value}]' has been removed`,
-			error: `Failed to remove an intent listener for '[${value}]'`,
-		},
-		receivedContextListener: {
-			info: `Received context via '[${value}]' listener`,
-			error: `Failed to receive context from '[${value}]' listener`,
-		},
-		receivedAppContextListener: {
-			info: `${value}`,
-			error: `${value}`,
-		},
-		addAppContextListener: {
-			info: `${value}`,
-			error: `${value}`,
-		},
-		receivedIntentListener: {
-			info: `Received context via '[${value}]' listener.`,
-			error: `Failed to receive context from '[${value}]' listener.`,
-		},
-		saveExample: {
-			success: `Saved context example for '[${value}]'.`,
-			error: `Failed to save context example.`,
-		},
-		deleteExample: {
-			success: `Deleted context example for '[${value}]'.`,
-			error: `Failed to delete context example.`,
-		},
-		copyToClipboard: {
-			info: `${value} code snippet copied to clipboard`,
-			error: `Failed to copy code ${value} snippet to clipboard.`,
-		},
-		getOrCreateChannel: {
-			success: `Retrieved or created current app channel [${value}]`,
-			error: `Failed to retrieve or create app channel ${value}.`,
-		},
-		createPrivateChannel: {
-			success: `Created current private channel [${value}]`,
-			error: `Failed to create private channel ${value}.`,
-		},
-		pcAddContextListener: {
-			success: `${value}`,
-			error: `${value}`,
-		},
-		pcOnUnsubscribe: {
-			success: `${value}`,
-			error: `${value}`,
-		},
-		pcOnDisconnect: {
-			success: `${value}`,
-			error: `${value}`,
-		},
-	};
+export const getLogMessage = (name: logMessagesName, type: logMessagesType, value: string = ''): string => {
+  const logMessages: LogMessages = {
+    getFdc3: {
+      error: `The FDC3 API is not ready${value ? ` (${value})` : ''}`,
+    },
+    getChannels: {
+      success: `Retrieved User channels`,
+      error: `Failed to retrieve User channels`,
+    },
+    getCurrentChannel: {
+      success: `Retrieved current channel [${value}]`,
+      error: `Failed to retrieve current channel`,
+    },
+    joinUserChannel: {
+      success: `Joined the [${value}] channel`,
+      error: `Failed to join the [${value}] channel`,
+    },
+    leaveChannel: {
+      success: `Left the [${value}] channel`,
+      error: `Failed to leave the [${value}] channel`,
+      warning: `Not currently joined to a channel`,
+    },
+    broadcast: {
+      success: 'Broadcast context',
+      error: 'Failed to broadcast context:',
+    },
+    appBroadcast: {
+      success: `Broadcast context to channel [${value}]: `,
+      error: `Failed to broadcast context to channel [${value}]:`,
+    },
+    raiseIntent: {
+      success: `Raised intent [${value}]`,
+      error: `Failed to raise intent [${value}]`,
+    },
+    raiseIntentForContext: {
+      success: `Raised intent for context`,
+      error: `Failed to raise intent for context`,
+    },
+    addContextListener: {
+      success: `A context listener for '[${value}]' has been added`,
+      error: `Failed to add a context listener for '[${value}]'`,
+    },
+    removeContextListener: {
+      success: `A context listener for '[${value}]' has been removed`,
+      error: `Failed to remove a context listener for '[${value}]'`,
+    },
+    removeAppChannelContextListener: {
+      success: `${value}`,
+      error: `${value}`,
+    },
+    addIntentListener: {
+      success: `An intent listener for '[${value}]' has been added`,
+      error: `Failed to add an intent listener for '[${value}]'`,
+    },
+    removeIntentListener: {
+      success: `An intent listener for '[${value}]' has been removed`,
+      error: `Failed to remove an intent listener for '[${value}]'`,
+    },
+    receivedContextListener: {
+      info: `Received context via '[${value}]' listener`,
+      error: `Failed to receive context from '[${value}]' listener`,
+    },
+    receivedAppContextListener: {
+      info: `${value}`,
+      error: `${value}`,
+    },
+    addAppContextListener: {
+      info: `${value}`,
+      error: `${value}`,
+    },
+    receivedIntentListener: {
+      info: `Received context via '[${value}]' listener.`,
+      error: `Failed to receive context from '[${value}]' listener.`,
+    },
+    saveExample: {
+      success: `Saved context example for '[${value}]'.`,
+      error: `Failed to save context example.`,
+    },
+    deleteExample: {
+      success: `Deleted context example for '[${value}]'.`,
+      error: `Failed to delete context example.`,
+    },
+    copyToClipboard: {
+      info: `${value} code snippet copied to clipboard`,
+      error: `Failed to copy code ${value} snippet to clipboard.`,
+    },
+    getOrCreateChannel: {
+      success: `Retrieved or created current app channel [${value}]`,
+      error: `Failed to retrieve or create app channel ${value}.`,
+    },
+    createPrivateChannel: {
+      success: `Created current private channel [${value}]`,
+      error: `Failed to create private channel ${value}.`,
+    },
+    pcAddContextListener: {
+      success: `${value}`,
+      error: `${value}`,
+    },
+    pcOnUnsubscribe: {
+      success: `${value}`,
+      error: `${value}`,
+    },
+    pcOnDisconnect: {
+      success: `${value}`,
+      error: `${value}`,
+    },
+  };
 
-	return logMessages[name][type] ?? (value != "" ? `${value}` : `Undefined log message ${name}.${type}`);
+  return logMessages[name][type] ?? (value != '' ? `${value}` : `Undefined log message ${name}.${type}`);
 };
--- toolbox/fdc3-workbench/src/store/IntentStore.ts
+++ toolbox/fdc3-workbench/src/store/IntentStore.ts
@@ -2,223 +2,223 @@
  * SPDX-License-Identifier: Apache-2.0
  * Copyright FINOS FDC3 contributors - see NOTICE file
  */
-import { makeObservable, observable, action, runInAction, toJS } from "mobx";
-import { ContextType, IntentResolution, Fdc3Listener, AppMetadata, PrivateChannel } from "../utility/Fdc3Api";
-import { nanoid } from "nanoid";
-import { intentTypes } from "../fixtures/intentTypes";
-import systemLogStore from "./SystemLogStore";
-import appChannelStore from "./AppChannelStore";
-import privateChannelStore from "./PrivateChannelStore";
-import { Channel, getAgent, IntentResult } from "@finos/fdc3";
+import { makeObservable, observable, action, runInAction, toJS } from 'mobx';
+import { ContextType, IntentResolution, Fdc3Listener, AppMetadata, PrivateChannel } from '../utility/Fdc3Api';
+import { nanoid } from 'nanoid';
+import { intentTypes } from '../fixtures/intentTypes';
+import systemLogStore from './SystemLogStore';
+import appChannelStore from './AppChannelStore';
+import privateChannelStore from './PrivateChannelStore';
+import { Channel, getAgent, IntentResult } from '@finos/fdc3';
 
 type IntentItem = { title: string; value: string };
 
 class IntentStore {
-	intentsList: IntentItem[] = intentTypes;
+  intentsList: IntentItem[] = intentTypes;
 
-	intentListeners: Fdc3Listener[] = [];
+  intentListeners: Fdc3Listener[] = [];
 
-	constructor() {
-		makeObservable(this, {
-			intentsList: observable,
-			intentListeners: observable,
-			raiseIntent: action,
-			addIntentListener: action,
-			removeIntentListener: action,
-			raiseIntentForContext: action,
-		});
-	}
+  constructor() {
+    makeObservable(this, {
+      intentsList: observable,
+      intentListeners: observable,
+      raiseIntent: action,
+      addIntentListener: action,
+      removeIntentListener: action,
+      raiseIntentForContext: action,
+    });
+  }
 
-	async addIntentListener(
-		intent: string,
-		resultContext?: ContextType | null,
-		channelName?: string | null,
-		isPrivate?: boolean,
-		channelContexts?: any,
-		channelContextDelay?: any
-	) {
-		try {
-			const listenerId = nanoid();
+  async addIntentListener(
+    intent: string,
+    resultContext?: ContextType | null,
+    channelName?: string | null,
+    isPrivate?: boolean,
+    channelContexts?: any,
+    channelContextDelay?: any
+  ) {
+    try {
+      const listenerId = nanoid();
 
-			const agent = await getAgent();
+      const agent = await getAgent();
 
-			const intentListener = await agent.addIntentListener(
-				intent,
-				async (context: ContextType, metaData?: any): Promise<IntentResult> => {
-					const currentListener = this.intentListeners.find(({ id }) => id === listenerId);
-					let channel: Channel | undefined;
+      const intentListener = await agent.addIntentListener(
+        intent,
+        async (context: ContextType, metaData?: any): Promise<IntentResult> => {
+          const currentListener = this.intentListeners.find(({ id }) => id === listenerId);
+          let channel: Channel | undefined;
 
-					//app channel
-					if (channelName && !isPrivate) {
-						channel = await appChannelStore.getOrCreateChannel(channelName);
-					}
+          //app channel
+          if (channelName && !isPrivate) {
+            channel = await appChannelStore.getOrCreateChannel(channelName);
+          }
 
-					//private channel
-					if (isPrivate && !channelName) {
-						channel = await privateChannelStore.createPrivateChannel();
-						privateChannelStore.addChannelListener(<PrivateChannel>channel, "all");
+          //private channel
+          if (isPrivate && !channelName) {
+            channel = await privateChannelStore.createPrivateChannel();
+            privateChannelStore.addChannelListener(<PrivateChannel>channel, 'all');
 
-						privateChannelStore.onDisconnect(<PrivateChannel>channel);
-						privateChannelStore.onUnsubscribe(<PrivateChannel>channel);
-						privateChannelStore.onAddContextListener(<PrivateChannel>channel, channelContexts, channelContextDelay);
-					}
+            privateChannelStore.onDisconnect(<PrivateChannel>channel);
+            privateChannelStore.onUnsubscribe(<PrivateChannel>channel);
+            privateChannelStore.onAddContextListener(<PrivateChannel>channel, channelContexts, channelContextDelay);
+          }
 
-					if (!isPrivate && channel) {
-						if (Object.keys(channelContexts).length !== 0) {
-							Object.keys(channelContexts).forEach((key) => {
-								let broadcast = setTimeout(async () => {
-									appChannelStore.broadcast(<Channel>channel, channelContexts[key]);
-									clearTimeout(broadcast);
-								}, channelContextDelay[key]);
-							});
-						} else {
-							await channel.broadcast(context);
-						}
-					}
+          if (!isPrivate && channel) {
+            if (Object.keys(channelContexts).length !== 0) {
+              Object.keys(channelContexts).forEach(key => {
+                let broadcast = setTimeout(async () => {
+                  appChannelStore.broadcast(<Channel>channel, channelContexts[key]);
+                  clearTimeout(broadcast);
+                }, channelContextDelay[key]);
+              });
+            } else {
+              await channel.broadcast(context);
+            }
+          }
 
-					runInAction(() => {
-						if (currentListener) {
-							currentListener.lastReceivedContext = context;
-							currentListener.metaData = metaData;
-						}
-					});
+          runInAction(() => {
+            if (currentListener) {
+              currentListener.lastReceivedContext = context;
+              currentListener.metaData = metaData;
+            }
+          });
 
-					systemLogStore.addLog({
-						name: "receivedIntentListener",
-						type: "info",
-						value: intent,
-						variant: "code",
-						body: JSON.stringify(context, null, 4),
-					});
+          systemLogStore.addLog({
+            name: 'receivedIntentListener',
+            type: 'info',
+            value: intent,
+            variant: 'code',
+            body: JSON.stringify(context, null, 4),
+          });
 
-					const result: IntentResult = channel || (resultContext ?? undefined);
+          const result: IntentResult = channel || (resultContext ?? undefined);
 
-					return result;
-				}
-			);
+          return result;
+        }
+      );
 
-			runInAction(() => {
-				systemLogStore.addLog({
-					name: "addIntentListener",
-					type: "success",
-					value: intent,
-					variant: "text",
-				});
-				this.intentListeners.push({ id: listenerId, type: intent, listener: intentListener });
-			});
-		} catch (e) {
-			systemLogStore.addLog({
-				name: "addIntentListener",
-				type: "error",
-				value: intent,
-				variant: "code",
-				body: JSON.stringify(e, null, 4),
-			});
-		}
-	}
+      runInAction(() => {
+        systemLogStore.addLog({
+          name: 'addIntentListener',
+          type: 'success',
+          value: intent,
+          variant: 'text',
+        });
+        this.intentListeners.push({ id: listenerId, type: intent, listener: intentListener });
+      });
+    } catch (e) {
+      systemLogStore.addLog({
+        name: 'addIntentListener',
+        type: 'error',
+        value: intent,
+        variant: 'code',
+        body: JSON.stringify(e, null, 4),
+      });
+    }
+  }
 
-	async removeIntentListener(id: string) {
-		const listenerIndex = this.intentListeners.findIndex((listener) => listener.id === id);
+  async removeIntentListener(id: string) {
+    const listenerIndex = this.intentListeners.findIndex(listener => listener.id === id);
 
-		if (listenerIndex !== -1) {
-			try {
-				(await this.intentListeners[listenerIndex].listener).unsubscribe();
+    if (listenerIndex !== -1) {
+      try {
+        (await this.intentListeners[listenerIndex].listener).unsubscribe();
 
-				runInAction(() => {
-					systemLogStore.addLog({
-						name: "removeIntentListener",
-						type: "success",
-						value: this.intentListeners[listenerIndex].type,
-						variant: "text",
-					});
-					this.intentListeners.splice(listenerIndex, 1);
-				});
-			} catch (e) {
-				systemLogStore.addLog({
-					name: "removeIntentListener",
-					type: "error",
-					value: this.intentListeners[listenerIndex].type,
-					variant: "code",
-					body: JSON.stringify(e, null, 4),
-				});
-			}
-		}
-	}
+        runInAction(() => {
+          systemLogStore.addLog({
+            name: 'removeIntentListener',
+            type: 'success',
+            value: this.intentListeners[listenerIndex].type,
+            variant: 'text',
+          });
+          this.intentListeners.splice(listenerIndex, 1);
+        });
+      } catch (e) {
+        systemLogStore.addLog({
+          name: 'removeIntentListener',
+          type: 'error',
+          value: this.intentListeners[listenerIndex].type,
+          variant: 'code',
+          body: JSON.stringify(e, null, 4),
+        });
+      }
+    }
+  }
 
-	async raiseIntent(intent: string, context: ContextType, app?: AppMetadata) {
-		const agent = await getAgent();
+  async raiseIntent(intent: string, context: ContextType, app?: AppMetadata) {
+    const agent = await getAgent();
 
-		if (!context) {
-			systemLogStore.addLog({
-				name: "raiseIntent",
-				type: "error",
-				value: intent,
-			});
-			return;
-		}
+    if (!context) {
+      systemLogStore.addLog({
+        name: 'raiseIntent',
+        type: 'error',
+        value: intent,
+      });
+      return;
+    }
 
-		try {
-			let resolution: IntentResolution;
+    try {
+      let resolution: IntentResolution;
 
-			if (app) {
-				resolution = await agent.raiseIntent(intent, toJS(context), app);
-			} else {
-				resolution = await agent.raiseIntent(intent, toJS(context));
-			}
+      if (app) {
+        resolution = await agent.raiseIntent(intent, toJS(context), app);
+      } else {
+        resolution = await agent.raiseIntent(intent, toJS(context));
+      }
 
-			systemLogStore.addLog({
-				name: "raiseIntent",
-				type: "success",
-				value: intent,
-				variant: "code",
-				body: JSON.stringify(resolution, null, 4),
-			});
+      systemLogStore.addLog({
+        name: 'raiseIntent',
+        type: 'success',
+        value: intent,
+        variant: 'code',
+        body: JSON.stringify(resolution, null, 4),
+      });
 
-			return resolution;
-		} catch (e) {
-			systemLogStore.addLog({
-				name: "raiseIntent",
-				type: "error",
-				value: intent,
-				variant: "code",
-				body: JSON.stringify(e, null, 4),
-			});
-		}
-	}
+      return resolution;
+    } catch (e) {
+      systemLogStore.addLog({
+        name: 'raiseIntent',
+        type: 'error',
+        value: intent,
+        variant: 'code',
+        body: JSON.stringify(e, null, 4),
+      });
+    }
+  }
 
-	async raiseIntentForContext(context: ContextType, app?: AppMetadata) {
-		const agent = await getAgent();
+  async raiseIntentForContext(context: ContextType, app?: AppMetadata) {
+    const agent = await getAgent();
 
-		if (!context) {
-			systemLogStore.addLog({
-				name: "raiseIntentForContext",
-				type: "error",
-			});
-			return;
-		}
+    if (!context) {
+      systemLogStore.addLog({
+        name: 'raiseIntentForContext',
+        type: 'error',
+      });
+      return;
+    }
 
-		try {
-			let resolution: IntentResolution;
+    try {
+      let resolution: IntentResolution;
 
-			if (app) {
-				resolution = await agent.raiseIntentForContext(toJS(context), app);
-			} else {
-				resolution = await agent.raiseIntentForContext(toJS(context));
-			}
-			systemLogStore.addLog({
-				name: "raiseIntentForContext",
-				type: "success",
-			});
+      if (app) {
+        resolution = await agent.raiseIntentForContext(toJS(context), app);
+      } else {
+        resolution = await agent.raiseIntentForContext(toJS(context));
+      }
+      systemLogStore.addLog({
+        name: 'raiseIntentForContext',
+        type: 'success',
+      });
 
-			return resolution;
-		} catch (e) {
-			console.log(e);
-			systemLogStore.addLog({
-				name: "raiseIntentForContext",
-				type: "error",
-			});
-		}
-	}
+      return resolution;
+    } catch (e) {
+      console.log(e);
+      systemLogStore.addLog({
+        name: 'raiseIntentForContext',
+        type: 'error',
+      });
+    }
+  }
 }
 
 const intentStore = new IntentStore();
