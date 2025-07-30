cdd2d998331ba52c995df4918998427de11589f4
Julianna Langston <julianna.langston@interop.io>
2025-07-30 15:03:16 -0400

    Consolidate ContextMetadata definitions

--- packages/fdc3-schema/generated/api/BrowserTypes.ts
+++ packages/fdc3-schema/generated/api/BrowserTypes.ts
@@ -6513,7 +6513,6 @@ const typeMap: any = {
   RaiseIntentResponseType: ['raiseIntentResponse'],
   RaiseIntentResultResponseType: ['raiseIntentResultResponse'],
 };
-
 export type AppRequestMessage =
   | AddContextListenerRequest
   | AddEventListenerRequest
