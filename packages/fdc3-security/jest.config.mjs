c85ddf37a153525ba975d4c867b1c2b76bd79e80
Rob Moffat <rob@kite9.com>
2026-02-18 13:48:57 +0000

    Working on getting tests working

--- packages/fdc3-security/jest.config.mjs
+++ packages/fdc3-security/jest.config.mjs
@@ -3,10 +3,27 @@ export default {
   preset: 'ts-jest/presets/default-esm',
   moduleFileExtensions: ['js', 'ts'],
   extensionsToTreatAsEsm: ['.ts'],
-  transformIgnorePatterns: ['node_modules/(?!jose)'],
+  transformIgnorePatterns: ['node_modules/(?!(jose|canonicalize)/)'],
   testRegex: '.+\\.test\\.ts?$',
   testEnvironment: 'node',
   reporters: ['default', 'jest-junit'],
   collectCoverage: true,
   coverageReporters: ['lcov', 'text', 'json'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: {
+          module: 'ESNext',
+          moduleResolution: 'node',
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
